2bd5ce8fbef752954b49cd96314466d854cd6904
굴소스 <139232765+GulSauce@users.noreply.github.com>
2023-11-22 02:11:24 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231122/두 사람/two-person.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int p1A;
+    char p1S; 
+    cin >> p1A >> p1S;
+
+    int p2A;
+    char p2S; 
+    cin >> p2A >> p2S;
+
+    cout << ((19 <= p1A || 19 <= p2A) && (p1S == 'M' || p2S == 'M'));
+    return 0;
+}
