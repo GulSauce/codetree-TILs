14563bd1b951d7236a71e91cf5891a19648cfe48
굴소스 <139232765+GulSauce@users.noreply.github.com>
2023-11-22 03:03:35 +0900

    Title: 일 수 구하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 231122/일 수 구하기/number-of-days-in-month.cpp
+++ 231122/일 수 구하기/number-of-days-in-month.cpp
@@ -6,7 +6,7 @@ int main() {
     int n; cin >> n;
     if(n == 2)
         cout << 28;
-    else if(n == 4 || n == 6 ||  n == 9)
+    else if(n == 4 || n == 6 ||  n == 9 || n == 11)
         cout << 30;
     else
         cout << 31;
