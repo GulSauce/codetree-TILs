938ebebe38a221047bdb330b9e37268967d15265
굴소스 <139232765+GulSauce@users.noreply.github.com>
2023-11-22 02:59:14 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231122/윤년인가/is-leap-year.cpp
+++ 231122/윤년인가/is-leap-year.cpp
@@ -4,7 +4,11 @@
 
 int main() {
     int y; cin >> y;
-    if(y % 400 == 0 || y % 4 == 0 && y % 100 != 0)
+    if(y % 400 == 0)
+        cout << "true";
+    else if(y % 100 == 0)
+        cout << "false";
+    else if(y % 4 == 0)
         cout << "true";
     else
         cout << "false";
