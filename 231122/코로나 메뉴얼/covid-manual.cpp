f2a42d627e2d65db506ca588193fe1cd98e617d7
굴소스 <139232765+GulSauce@users.noreply.github.com>
2023-11-22 03:18:02 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231122/코로나 메뉴얼/covid-manual.cpp
+++ 231122/코로나 메뉴얼/covid-manual.cpp
@@ -7,7 +7,6 @@ int main() {
     char disB; int tempB; cin >> disB >> tempB;
     char disC; int tempC; cin >> disC >> tempC;
 
-    int aCnt = 0;
     if(disA == 'Y' && tempA >= 37){
         if(disB == 'Y' && tempB >= 37 || disC == 'Y' && tempC >= 37) 
             cout << "E";
@@ -15,11 +14,11 @@ int main() {
             cout << "N";
     }
     
-    else if(disB == 'Y' && tempB >= 37 || disC == 'Y' && tempC >= 37){
+    else if(disB == 'Y' && tempB >= 37 && disC == 'Y' && tempC >= 37)
         cout << "E";
-    }
 
     else
         cout << "N";
+    
     return 0;
 }
