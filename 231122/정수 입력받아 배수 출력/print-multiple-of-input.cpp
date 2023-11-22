322c5ead5055bb394395f46325e85a25c4211e41
굴소스 <139232765+GulSauce@users.noreply.github.com>
2023-11-22 16:00:02 +0900

    Title: 정수 입력받아 배수 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231122/정수 입력받아 배수 출력/print-multiple-of-input.cpp
+++ 231122/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     int n; cin >> n;
-    for(int i = 1; i <= 5; i ++)
-        cout << i*n << ' ';
+    for(int i = n; i <= 5 * n; i += n)
+        cout << i << ' ';
     return 0;
 }
