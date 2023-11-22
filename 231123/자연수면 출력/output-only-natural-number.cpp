1a0e7b12106a5a191bc479bd10d3c1a2f2c88b71
굴소스 <139232765+GulSauce@users.noreply.github.com>
2023-11-23 02:19:38 +0900

    Title: 자연수면 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231123/자연수면 출력/output-only-natural-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b; cin >> a >> b;
+    if(1 <= a){
+        while(b--)
+            cout << a;
+    }
+    else
+        cout << 0;
+        
+    return 0;
+}
