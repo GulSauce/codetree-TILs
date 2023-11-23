b73356ffd01757a389d5bfcf591478d577a33b74
굴소스 <139232765+GulSauce@users.noreply.github.com>
2023-11-23 14:51:04 +0900

    Title: 청소당번 정하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231123/청소당번 정하기/cleaning-numbering.cpp
+++ 231123/청소당번 정하기/cleaning-numbering.cpp
@@ -9,13 +9,13 @@ int main() {
     int floor = 0;
     int toilet = 0;
 
-    for(int i = 2; i < n; i++){
-        if((i-1) % 2 == 0)
-            classRoom++;
-        if((i-1) % 3 == 0)
-            floor++;
-        if((i-1) % 12 == 0)
+    for(int i = 1; i < n; i++){
+        if((i+1) % 12 == 0)
             toilet++;
+        else if((i+1) % 3 == 0)
+            floor++;
+        else if((i+1) % 2 == 0)
+            classRoom++;
     }
 
     cout << classRoom << ' ' << floor << ' ' << toilet;
