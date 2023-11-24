ddeae2d781976fa309268358013fc146a3835a31
굴소스 <139232765+GulSauce@users.noreply.github.com>
2023-11-24 15:42:25 +0900

    Title: 등장횟수 확인, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231124/등장횟수 확인/check-number-of-appearances.cpp
+++ 231124/등장횟수 확인/check-number-of-appearances.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main() {
     int cnt = 0;
     for(int i = 1; i <= 5; i++){
-        if(i % 2 == 0)
+        int num; cin >> num;
+        if(num % 2 == 0)
             cnt++;
     }
 
