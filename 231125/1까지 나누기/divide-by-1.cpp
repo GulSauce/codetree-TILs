7ac1ddb47cb59f03d4ae2b11d68f36b5076fd3d1
굴소스 <139232765+GulSauce@users.noreply.github.com>
2023-11-25 17:56:47 +0900

    Title: 1까지 나누기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231125/1까지 나누기/divide-by-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    int cnt = 0;
+    for(int i =1 ; ; i++){
+        n /= i;
+        cnt++;
+        if(n <= 1){
+            cout << cnt;
+            break;
+        }   
+    }
+    return 0;
+}
