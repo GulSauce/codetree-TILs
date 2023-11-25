7065398fcdf03bab29c5dd8245ce5b0a7b7da54d
굴소스 <139232765+GulSauce@users.noreply.github.com>
2023-11-25 19:48:19 +0900

    Title: 조건에 따른 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231125/조건에 따른 연산/operatino-by-rule.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    int cnt = 0;
+    while(1){
+        if(n % 2 == 0)
+            n = n * 3 +1;
+        else
+            n = n * 2 + 2;
+        cnt++;
+        if(n >= 1000)
+            break;
+    }
+    cout << cnt;
+    return 0;
+}
