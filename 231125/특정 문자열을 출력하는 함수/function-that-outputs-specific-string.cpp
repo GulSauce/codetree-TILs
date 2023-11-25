56e54eb208ed0a2247fdebb07b036870306814b1
굴소스 <139232765+GulSauce@users.noreply.github.com>
2023-11-25 20:07:20 +0900

    Title: 특정 문자열을 출력하는 함수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231125/특정 문자열을 출력하는 함수/function-that-outputs-specific-string.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    while(n--){
+        cout << "Hello\n";
+        cout << "#@#@#@#@#@\n";
+        cout << "CodeTree\n";
+        cout << "@#@#@#@#@#\n";
+        cout << "Students!\n";
+        cout << '\n';
+    }
+    return 0;
+}
