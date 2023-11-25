9684ad60abbf7f253a343b746983abb08cb6f264
굴소스 <139232765+GulSauce@users.noreply.github.com>
2023-11-25 17:47:33 +0900

    Title: 조건에 부합하는 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231125/조건에 부합하는 수/number-that-meets-the-condition.cpp
+++ 231125/조건에 부합하는 수/number-that-meets-the-condition.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main() {
     int a; cin >> a;
-    for(int i =1 ; i<= a; i++){
+    for(int i = 1 ; i<= a; i++){
         if(i % 2 == 0 && i % 4 != 0)
             continue;
-        if(i % 8 == 0)
+        if(i / 8 % 2 == 0)
             continue;
         if(i % 7 < 4)
             continue;
