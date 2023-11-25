9060ce88868e96081d852dfe4233c54881455810
굴소스 <139232765+GulSauce@users.noreply.github.com>
2023-11-25 19:52:22 +0900

    Title: 합성수 판별기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231125/합성수 판별기/factorization-discriminator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    bool satisfied = false;
+    for(int i = 2; i < n; i++){
+        if(n % 2 == 0)
+            satisfied = true;
+    }
+    if(satisfied)
+        cout << 'C';
+    else
+        cout << 'N';
+    return 0;
+}
