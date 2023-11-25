6575e6ed1b5281f2a46b89a68ee40a7856042633
굴소스 <139232765+GulSauce@users.noreply.github.com>
2023-11-26 00:49:20 +0900

    Title: 별표 출력하기 5, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231126/별표 출력하기 5/print-star-5.cpp
+++ 231126/별표 출력하기 5/print-star-5.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main() {
     int n; cin >> n;
-    for(int y = 0; y < n; y++){
-        for(int i = 0; i < n-y; i++){
-            for(int x = 0; x < n-y; x++)
+    for(int y = n; y > 0; y--){
+        for(int i = 0; i < y; i++){
+            for(int x = 0; x < y; x++)
                 cout << '*';
             cout << ' ';
         }
