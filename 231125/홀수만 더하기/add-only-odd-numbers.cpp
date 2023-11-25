748cb4d032f73ab8c2dec159208c6d0cb2f79e2d
굴소스 <139232765+GulSauce@users.noreply.github.com>
2023-11-25 16:00:53 +0900

    Title: 홀수만 더하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231125/홀수만 더하기/add-only-odd-numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    int sumVal = 0;
+    while(n--){
+        int num; cin >> num;
+        if(num % 3 == 0)
+            sumVal += num;
+    }
+
+    cout << sumVal;
+    return 0;
+}
