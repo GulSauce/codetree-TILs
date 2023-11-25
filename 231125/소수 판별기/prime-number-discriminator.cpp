d009babbf675ea3db5999a94decb404128e1a5fc
굴소스 <139232765+GulSauce@users.noreply.github.com>
2023-11-25 20:01:51 +0900

    Title: 소수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231125/소수 판별기/prime-number-discriminator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    bool flag = true;
+    for(int i = 2; i < n; i++){
+        if(n % i == 0)
+            flag = false;
+    } 
+    if(flag)
+        cout << 'P';
+    else
+        cout << 'C';
+    return 0;
+}
