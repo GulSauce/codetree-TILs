285f7d2dc3f6cc8094d5958124274eef4f1d4929
굴소스 <139232765+GulSauce@users.noreply.github.com>
2023-11-27 02:33:43 +0900

    Title: 별 그리기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231127/별 그리기/star-drawing.cpp
+++ 231127/별 그리기/star-drawing.cpp
@@ -5,14 +5,14 @@ using namespace std;
 int main() {
     int n; cin >> n;
     for(int i = 0 ; i < n; i++){
-        for(int b = 0; b < n-i; b++)
+        for(int b = 0; b < n - i - 1; b++)
             cout << ' ';
-        for(int s = 0; s < 2*i+1; s++)
+        for(int s = 0; s < 2 * i + 1; s++)
             cout << '*';
         cout << '\n';
     }
     for(int i = 0 ; i < n-1; i++){
-        for(int b = 0; b <= i+1; b++)
+        for(int b = 0; b <= i; b++)
             cout << ' ';
         for(int s = 0; s < 2 * (n - 1 - i) - 1; s++)
             cout << '*';
