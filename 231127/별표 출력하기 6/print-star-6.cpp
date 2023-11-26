06585688d9da13a4bdfe6248bfdc30bf4939a749
굴소스 <139232765+GulSauce@users.noreply.github.com>
2023-11-27 02:24:56 +0900

    Title: 별표 출력하기 6, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231127/별표 출력하기 6/print-star-6.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    for(int i = 0; i < n; i++){
+        for(int b = 0; b < i; b++)
+            cout << "  ";
+        for(int s = 0; s < 2*(n-i)-1; s++)
+            cout << "* ";
+        cout << '\n';
+    }
+
+    for(int i = 1; i < n; i++){
+        for(int b = 0; b < n- i - 1; b++)
+            cout << "  ";
+        for(int s = 0; s < 2 * i + 1; s++)
+            cout << "* ";
+        cout << '\n';
+    }
+    return 0;
+}
