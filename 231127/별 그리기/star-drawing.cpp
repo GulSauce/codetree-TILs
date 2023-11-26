decb952a4bd74ad3b42796806152bb1d9f9ea117
굴소스 <139232765+GulSauce@users.noreply.github.com>
2023-11-27 02:32:21 +0900

    Title: 별 그리기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231127/별 그리기/star-drawing.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    for(int i = 0 ; i < n; i++){
+        for(int b = 0; b < n-i; b++)
+            cout << ' ';
+        for(int s = 0; s < 2*i+1; s++)
+            cout << '*';
+        cout << '\n';
+    }
+    for(int i = 0 ; i < n-1; i++){
+        for(int b = 0; b <= i+1; b++)
+            cout << ' ';
+        for(int s = 0; s < 2 * (n - 1 - i) - 1; s++)
+            cout << '*';
+        cout << '\n';
+    }
+    return 0;
+}
