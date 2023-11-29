5036b9c871605a2fadb88a74c9e7dce9d2d4ce60
굴소스 <139232765+GulSauce@users.noreply.github.com>
2023-11-30 02:12:29 +0900

    Title: 행 단위로 반복하여 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231130/행 단위로 반복하여 출력/print-in-row.cpp
+++ 231130/행 단위로 반복하여 출력/print-in-row.cpp
@@ -6,7 +6,7 @@ int main() {
     int n; cin >> n;
     for(int i = 0; i < n; i++){
         for(int j = 1; j <= 3; j++)
-            cout << n;
+            cout << j;
         cout << '\n';
     }
     return 0;
