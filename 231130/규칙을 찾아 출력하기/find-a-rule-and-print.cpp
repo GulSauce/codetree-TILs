4c2b48f721926ec87821e72af0a07982c6033002
굴소스 <139232765+GulSauce@users.noreply.github.com>
2023-11-30 01:17:35 +0900

    Title: 규칙을 찾아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231130/규칙을 찾아 출력하기/find-a-rule-and-print.cpp
+++ 231130/규칙을 찾아 출력하기/find-a-rule-and-print.cpp
@@ -4,17 +4,13 @@ using namespace std;
 
 int main() {
     int n; cin >> n;
-    for(int i = 0; i < n; i++)
-        cout << "* ";
-    cout << '\n';
-    for(int i = 1; i < n; i++){
-        for(int s = 0; s < n-1; s++){
-            if(s < i)
+    for(int i = 0; i < n; i++){
+        for(int s = 0; s < n; s++){
+            if(i == 0 || s == n-1 || s < i)
                 cout << "* ";
             else
                 cout << "  "; 
         }
-        cout << "*";
         cout << '\n';
     }
     return 0;
