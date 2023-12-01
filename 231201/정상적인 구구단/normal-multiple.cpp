6baf69025fa395caad1889babe15e3b31b371c0b
굴소스 <139232765+GulSauce@users.noreply.github.com>
2023-12-01 18:47:37 +0900

    Title: 정상적인 구구단, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231201/정상적인 구구단/normal-multiple.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    for(int y =1; y <= n; y++){
+        for(int x =1; x <= n; x++){
+            cout << y << " * " << x << " = " << y*x;
+            if(x < n-1)
+                cout << ", "
+            else
+                cout << '\n';
+        }
+    }
+    return 0;
+}
