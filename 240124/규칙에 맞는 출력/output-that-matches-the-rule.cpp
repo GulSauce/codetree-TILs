a7db099c928524fb01dda150b20a2c801b4275b0
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-01-24 17:40:09 +0900

    Title: 규칙에 맞는 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240124/규칙에 맞는 출력/output-that-matches-the-rule.cpp
+++ 240124/규칙에 맞는 출력/output-that-matches-the-rule.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     int n; cin >> n;
     for(int y = 0; y < n; y++){
-        for(int x = n-y; x <= 5; x++){
+        for(int x = n-y; x <= n; x++){
             cout << x << ' ';
         }
         cout << '\n';
