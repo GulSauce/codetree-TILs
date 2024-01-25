5be22d2ba4b86c0f6cedcff067f49fd0841ef18c
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-01-25 18:52:07 +0900

    Title: 1-9 개수 세기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240125/1-9 개수 세기/count-one-to-nine.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int numberCount[10];
+    int n; cin >> n;
+    while(n--){
+        int num; cin >> num;
+        numberCount[num]++;
+    }
+
+    for(int i = 1; i <= 9; i++)[
+        cout << numberCount[i] << '\n';
+    ]
+    return 0;
+}
