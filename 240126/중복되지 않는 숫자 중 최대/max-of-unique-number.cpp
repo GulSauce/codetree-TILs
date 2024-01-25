1934a916aae87290cc73070f20ffabc5606b4755
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-01-26 04:55:44 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240126/중복되지 않는 숫자 중 최대/max-of-unique-number.cpp
+++ 240126/중복되지 않는 숫자 중 최대/max-of-unique-number.cpp
@@ -5,15 +5,15 @@ using namespace std;
 int main() {
     int N; cin >> N;
 
-    int nums[1001] = {};
+    int numCount[1001] = {};
     while(N--){
         int num; cin >> num;
-        nums[num]++;
+        numCount[num]++;
     }
 
     int maxVal = -1;
     for(int i = 1; i <= 1000; i++){
-        if(0 == nums[i] || 2 <= nums[i]){
+        if(0 == numCount[i] || 2 <= numCount[i]){
             continue;
         }
 
