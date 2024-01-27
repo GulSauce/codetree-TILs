54702a20568498eec87caeb0483fc3d15e1899ac
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-01-27 18:06:19 +0900

    Title: 격자 반대로 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240127/격자 반대로 채우기/grid-anti-fill.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int matrix[10][10];
+    
+    int increaseVal = 1;
+
+    int n; cin >> n;
+    for(int x = n-1; x >= 0; x--){
+        if(x % 2 == 0){
+            for(int y = 0; y <= n-1; y++){
+                matrix[y][x] = increaseVal++;
+            }
+        }
+
+        if(x % 2 == 1){
+            for(int y = n-1; y >= 0; y--){
+                matrix[y][x] = increaseVal++;
+            }
+        }
+    }
+
+    for(int y = 0; y <= n-1; y++){
+        for(int x = 0; x <= n-1; x++){
+            cout << matrix[y][x] << ' ';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
