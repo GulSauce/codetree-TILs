eda212c91a04c24a8f1ef2c3776f3249d1e260d9
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-01-29 21:51:40 +0900

    Title: 동전이 있는 위치, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240129/동전이 있는 위치/Where-coins-are-located.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n,  m; cin >> n >> m;
+    int coins[10][10];
+    while(m--){
+        int r, c; cin >> r >> c;
+        coins[r][c] = 1;
+    }
+    for(int y = 1; y <= n; y++){
+        for(int x = 1; x <= n; x++){
+            cout << coins[y][x] << ' ';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
