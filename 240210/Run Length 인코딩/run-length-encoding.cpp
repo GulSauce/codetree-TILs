52ca4d7d9e557da185456ceb4bdb09b69f36377b
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-02-10 12:29:53 +0900

    Title: Run Length 인코딩, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240210/Run Length 인코딩/run-length-encoding.cpp
+++ 240210/Run Length 인코딩/run-length-encoding.cpp
@@ -7,17 +7,24 @@ int main() {
     string str; cin >> str;
     string RLE = "";
     
-    for(int i = 0; str[i] != '\0';){
-        int j = i;
-        int cnt = 0;
-        while(str[i] == str[j]){
-            j++;
-            cnt++;
+    char curTarget = str[0];
+    int curCount = 1;
+
+    for(int i = 1; str[i] != '\0'; i++){
+        if(curTarget == str[i]){
+            curCount++;
+            continue;
         }
-        RLE += str[i];
-        RLE += to_string(cnt);
-        i = j;
+        
+        RLE += curTarget;
+        RLE += to_string(curCount);
+
+        curTarget = str[i];
+        curCount = 1;
     }
+
+    RLE += curTarget;
+    RLE += to_string(curCount);
 
     cout << RLE.length() << '\n' << RLE;
     return 0;
