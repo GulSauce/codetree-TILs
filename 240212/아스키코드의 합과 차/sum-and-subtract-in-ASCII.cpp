7beb8ffbc6fe049087d40dbc0b9f06db87bfbb14
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-02-12 15:28:10 +0900

    Title: 아스키코드의 합과 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240212/아스키코드의 합과 차/sum-and-subtract-in-ASCII.cpp
+++ 240212/아스키코드의 합과 차/sum-and-subtract-in-ASCII.cpp
@@ -5,14 +5,17 @@ using namespace std;
 int main() {
     char eng1, eng2;
     cin >> eng1 >> eng2;
-    
+
+    int eng1Value = (int) eng1;
+    int eng2Value = (int) eng2;
+     
     int sub;
-    if(eng1 <= eng2){
-        sub = eng2 - eng1;
+    if(eng1Value <= eng2Value){
+        sub = eng2Value - eng1Value;
     } else{
-        sub = eng1- eng2;
+        sub = eng1Value- eng2Value;
     }
 
-    cout << eng1 + eng2 << ' ' << sub;
+    cout << eng1Value + eng2Value << ' ' << sub;
     return 0;
 }
