e32c17e55fc9d09bbf70c43caf0f89ecf3fc48aa
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-02-15 12:08:54 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240215/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int n1, n2;
+int A[100], B[100]; 
+
+bool checkIsPartialPermutaion(){
+    bool matched = false;
+    for(int i = 0; i <= n1-n2; i++){
+        bool curMatched = true;
+        for(int j = 0; j <= n2-1; j++){
+            if(A[i+j] != B[j]){
+                curMatched = false;
+            }
+        }
+        matched = curMatched;
+        if(matched){
+            break;
+        }
+    }
+    return matched;
+}
+
+int main() {
+
+    cin >> n1 >> n2;
+
+    for(int i = 0; i <= n1-1; i++){
+        cin >> A[i];
+    }
+    for(int i = 0; i <= n2-1; i++){
+        cin >> B[i];
+    }
+
+    bool matched = checkIsPartialPermutaion();
+    if(matched){
+        cout << "Yes";
+    }
+    else{
+        cout << "No";
+    }
+    return 0;
+}
