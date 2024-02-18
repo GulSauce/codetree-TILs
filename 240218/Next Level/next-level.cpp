c8138796b20215e2223b4079f6acb5f611ed5f71
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-02-18 18:02:49 +0900

    Title: Next Level, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240218/Next Level/next-level.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <tuple>
+
+using namespace std;
+
+int main() {
+    pair<string, int> userInfo[2];
+    userInfo[0] = {"codetree", 10};
+
+    string userId; 
+    int userLevel;
+    cin >> userId >> userLevel;
+
+    userInfo[1] = {userId, userLevel};
+
+    string id[2];
+    int level[2];
+
+    for(int i = 0; i <= 1; i++){
+        id[i] = userInfo[i].first;
+        level[i] = userInfo[i].second;  
+    }
+
+    for(int i = 0; i <= 1; i++){
+        cout << "user " << id[i] << ' ' << "lv " << level[i] << '\n'; 
+    }
+    
+    return 0;
+}
