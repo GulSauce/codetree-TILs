4ab3ec018830ba0250e23f81bace12a04a367a09
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-02-20 18:51:11 +0900

    Title: 요일 맞추기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240220/요일 맞추기/guess-day-of-week.cpp
+++ 240220/요일 맞추기/guess-day-of-week.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
 
 using namespace std;
 
 int daysOfMonth[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 int daySumOfMonth[13] = {0};
-string dayToString[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+string dayToString[7] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
 
 void calcDaySumOfMonth(){
     for(int i = 2; i <=12; i++){
@@ -20,10 +19,14 @@ int main() {
 
     calcDaySumOfMonth();  
 
-    int m1Days = daySumOfMonth[m1] + daysOfMonth[m1];
-    int m2Days = daySumOfMonth[m2] + daysOfMonth[m2];
+    int m1Days = daySumOfMonth[m1] + d1;
+    int m2Days = daySumOfMonth[m2] + d2;
 
-    int dayDiff = abs(m1Days-m2Days);
+    int dayDiff = m2Days-m1Days;
+
+    if(dayDiff <= 0){
+        dayDiff += 7;
+    }
 
     dayDiff %= 7;
 
