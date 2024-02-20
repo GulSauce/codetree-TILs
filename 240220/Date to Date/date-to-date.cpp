9a923dc0a8ccdbb878fe7f847c1dc08307a668ad
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-02-20 16:43:00 +0900

    Title: Date to Date, Time: 3ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240220/Date to Date/date-to-date.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int daysOfMonths[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; 
+int daySumOfMonths[13] = {0};
+
+void calcDaySumofMonths(){
+    for(int i = 2; i <= 12; i++){
+        daySumOfMonths[i] = daySumOfMonths[i-1] + daysOfMonths[i];
+    }
+}
+
+int main() {
+    int m1, d1, m2, d2;
+    cin >> m1 >> d1 >> m2 >> d2;
+
+    calcDaySumofMonths();
+
+    int mDiff = daySumOfMonths[m2]-daySumOfMonths[m1];
+    int dDiff = d2 - d1;
+    
+    int result = mDiff + dDiff;
+    cout << result;
+    return 0;
+}
