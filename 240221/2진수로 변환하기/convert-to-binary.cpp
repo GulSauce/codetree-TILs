bf3a7d04d8dda54f5476a15538f6c671bf54a503
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-02-21 14:27:04 +0900

    Title: 2진수로 변환하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240221/2진수로 변환하기/convert-to-binary.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n; 
+    cin >> n;
+
+    int decimalToBinary[100];
+    int digitCnt = 0;
+    while(n){
+        decimalToBinary[digitCnt++] = n % 2;
+        n /= 2;
+    }
+
+    for(int i = digitCnt-1; i >= 0; i--){
+        cout << decimalToBinary[i];
+    }
+    return 0;
+}
