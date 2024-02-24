bcc720065959f9bd8bb1331a1a9d7dc1a2bfea7f
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-02-25 02:33:00 +0900

    Title: 흰검 칠하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240225/흰검 칠하기/painting-white-black.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+pair<int, int> boardColorCount[200000];
+char boardColor[200000];
+
+void moveLeft(int& curPos, int moveCount){
+    curPos -= moveCount;
+    for(int i = 0; i <= moveCount-1; i++){
+        boardColor[curPos+i] = 'W';
+        boardColorCount[curPos+i].first++;
+    }
+}
+
+void moveRight(int& curPos, int moveCount){
+    while(moveCount--){
+        boardColor[curPos] = 'B';
+        boardColorCount[curPos].second++;
+        curPos++;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int curPos = 100000;
+    while(n--){
+        int moveCount;
+        char direction;
+        cin >> moveCount >> direction;
+        if(direction == 'L'){
+            moveLeft(curPos, moveCount);
+        }
+        if(direction == 'R'){
+            moveRight(curPos, moveCount);
+        }
+    }
+
+    int w = 0, b = 0, g = 0;
+    for(int i = 0; i <= 199999; i++){
+        if(2 <= boardColorCount[i].first && 2 <= boardColorCount[i].second){
+            g++;
+        }
+        else{
+            if(boardColor[i] == 'W'){
+                w++;
+            }
+            if(boardColor[i] == 'B'){
+                b++;
+            }
+        }
+    }
+
+    cout << w << ' ' << b << ' ' << g;
+    return 0;
+}
