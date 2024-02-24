524f1cce7aa62e4ae0b15038203f6738c840b87b
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-02-24 19:41:58 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240224/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int board[2001];
+
+void moveLeft(int& currentPosition, int count){
+    while(count--){
+        board[currentPosition--]++;
+    }
+}
+
+void moveRight(int& currentPosition, int count){
+    while(count--){
+        board[currentPosition++]++;
+    }
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    int currentPosition = 1000;
+    while(n--){
+        int x;
+        char direction;
+        cin >> x >> direction;
+        if(direction == 'L'){
+            moveLeft(currentPosition, x);
+        }
+        if(direction == 'R'){
+            moveRight(currentPosition, x);
+        }
+    }
+
+    int area = 0;
+    for(int i = 0; i <= 2000; i++){
+        if(2 <= board[i]){
+            area++;
+        }
+    }
+
+    cout << area;
+    return 0;
+}
