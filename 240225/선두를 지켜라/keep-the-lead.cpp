be77a7415133be95434afdf8f46bc4359057712a
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-02-25 21:33:29 +0900

    Title: 선두를 지켜라, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240225/선두를 지켜라/keep-the-lead.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+
+using namespace std;
+
+int aLegnthAtHour[100001];
+int bLegnthAtHour[100001];
+
+void runA(int& currentAPostion, int& currentATime, int v, int t){
+    while(t--){
+        currentAPostion += v;
+        aLegnthAtHour[++currentATime] = currentAPostion;
+    }
+}
+
+void runB(int& currentBPostion, int& currentBTime, int v, int t){
+    while(t--){
+        currentBPostion += v;
+        bLegnthAtHour[++currentBTime] = currentBPostion;
+    }
+}
+
+int calcChangeCount(int totalTime){
+    char whoIsFirst = '\0';
+    int cnt = 0;
+    if(bLegnthAtHour[1] < aLegnthAtHour[1]){
+        whoIsFirst = 'a';
+    }
+    if(aLegnthAtHour[1] < bLegnthAtHour[1]){
+        whoIsFirst = 'b';
+    }
+ 
+    for(int t = 2; t <= totalTime; t++){
+        if(whoIsFirst == 'a'){
+            if(aLegnthAtHour[t] < bLegnthAtHour[t]){
+                whoIsFirst = 'b';
+                cnt++;
+            }
+        }
+        if(whoIsFirst == 'b'){
+            if(bLegnthAtHour[t] < aLegnthAtHour[t]){
+                whoIsFirst = 'a';
+                cnt++;
+            }
+        }
+        if(whoIsFirst == '\0'){
+            if(aLegnthAtHour[t] < bLegnthAtHour[t]){
+                whoIsFirst = 'b';
+                cnt++;
+            }
+            if(bLegnthAtHour[t] < aLegnthAtHour[t]){
+                whoIsFirst = 'a';
+                cnt++;
+            }
+        }
+        if(bLegnthAtHour[t] == aLegnthAtHour[t]){
+            whoIsFirst = '\0';
+        }
+    }
+    return cnt;
+}
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    int currentAPostion = 0;
+    int currentATime = 0;
+
+    while(N--){
+        int v, t;
+        cin >> v >> t;
+        runA(currentAPostion, currentATime, v, t);
+    }
+
+    int currentBPostion = 0;
+    int currentBTime = 0;
+
+    while(M--){
+        int v, t;
+        cin >> v >> t;
+        runB(currentBPostion, currentBTime, v, t);
+    }
+
+    int ans = calcChangeCount(currentATime);
+    cout << ans;
+    return 0;
+}
