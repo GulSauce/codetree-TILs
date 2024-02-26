0faba0f81992827a0fedd9c46741d4670b339d75
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-02-26 21:59:35 +0900

    Title: 작은 구슬의 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240226/작은 구슬의 이동/small-marble-movement.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int dx[4] = {1, 0, 0, -1};
+int dy[4] = {0, 1, -1, 0};
+
+int n, t;
+
+bool checkIsInBorder(int x, int y){
+    return x == 0 || x == n+1 || y == 0 || y == n+1;
+}
+
+int main() {
+    cin >> n >> t;
+    int r, c;
+    char d;
+    cin >> r >> c >> d;
+
+    int currentDirectionIndex = -1;
+    if(d == 'U'){
+        currentDirectionIndex = 1;
+    }
+    if(d == 'D'){
+        currentDirectionIndex = 2; 
+    }
+    if(d == 'R'){
+        currentDirectionIndex = 0;
+    }
+    if(d == 'L'){
+        currentDirectionIndex = 3;
+    }
+
+    while(t--){
+        if(checkIsInBorder(c, r)){
+            currentDirectionIndex = 3 - currentDirectionIndex;
+            t++;
+        }
+        c += dx[currentDirectionIndex];
+        r += dy[currentDirectionIndex];
+    }
+    
+    cout << r << ' ' << c;
+    return 0;
+}
