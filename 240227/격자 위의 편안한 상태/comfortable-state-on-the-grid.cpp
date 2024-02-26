437e283a92facff38a02d9e3945654ede2fabc85
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-02-27 02:28:18 +0900

    Title: 격자 위의 편안한 상태, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240227/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+bool isComfortable[100];
+bool board[101][101];
+
+int N, M;
+
+int dx[4] = {1, 0, -1 ,0};
+int dy[4] = {0, -1, 0 ,1};
+
+bool isInRange(int y, int x){
+    return 1 <= y && y <= N && 1 <= x && x <= N;
+}
+
+bool checkIsComfortable(int row, int column){
+    int coloredCnt = 0;
+    for(int i = 0; i <= 3; i++){
+        int nextY = row + dy[i];
+        int nextX = column + dx[i];
+        
+        if(!isInRange(nextY, nextX)){
+            continue;
+        }
+        if(board[nextY][nextX]){
+            coloredCnt++;
+        }
+    }
+
+    if(coloredCnt == 3){
+        return true;
+    }
+
+    return false;
+}
+
+int main() {
+    cin >> N >> M;
+    for(int i = 0; i <= M-1; i++){
+        int r, c;
+        cin >> r >> c;
+        board[r][c] = true;
+        if(checkIsComfortable(r, c)){
+            isComfortable[i] = true;
+        }
+    }
+    for(int i = 0; i <= M-1; i++){
+        if(isComfortable[i]){
+            cout << 1;
+        }else{
+            cout << 0;
+        }
+        cout << '\n';
+    }
+    return 0;
+}
