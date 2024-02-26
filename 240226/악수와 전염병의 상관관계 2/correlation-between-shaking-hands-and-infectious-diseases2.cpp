8ffc8ad9093f0dbf1bde5c7e40bcbfc692642bc0
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-02-26 18:20:34 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240226/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+bool isInfected[101];
+int shakeHandCount[101];
+
+class ShakeHandInfo{
+    public:
+        int time;
+        int dev1;
+        int dev2;
+        ShakeHandInfo(int time, int dev1, int dev2){
+            this->time = time;
+            this->dev1 = dev1;
+            this->dev2 = dev2;
+        }
+
+        ShakeHandInfo(){}
+};
+
+bool cmp(const ShakeHandInfo& a, const ShakeHandInfo& b){
+     return a.time < b.time;
+}
+
+int main() {
+    int N, K, P, T;
+    cin >> N >> K >> P >> T;
+
+    isInfected[P] = true;
+    ShakeHandInfo shakeHandInfo[250];
+
+    for(int i = 0; i <= T-1; i++){
+        int t, x, y;
+        cin >> t >> x >>  y;
+        shakeHandInfo[i] = ShakeHandInfo(t, x, y);
+    }
+
+    sort(shakeHandInfo, shakeHandInfo+T, cmp);
+
+    for(int i = 0; i <= T-1; i++){
+        int dev1 = shakeHandInfo[i].dev1;
+        int dev2 = shakeHandInfo[i].dev2;
+        if(isInfected[dev1]){
+            if(K <= shakeHandCount[dev1]){
+                continue;                
+            }
+            shakeHandCount[dev1]++;
+            isInfected[dev2] = true;
+        }
+        if(isInfected[dev2]){
+            if(K <= shakeHandCount[dev2]){
+                continue;                
+            }
+            shakeHandCount[dev2]++;
+            isInfected[dev1] = true;
+        }
+    }
+
+    for(int i = 1; i <= N; i++){
+        if(isInfected[i]){
+            cout << 1;
+        }else{
+            cout << 0;
+        }
+    }
+
+    return 0;
+}
