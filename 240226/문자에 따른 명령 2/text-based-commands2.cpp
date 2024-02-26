af57a5ad45b1294b8e6a7c0602f2008ddbb499fa
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-02-26 20:59:24 +0900

    Title: 문자에 따른 명령 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240226/문자에 따른 명령 2/text-based-commands2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, -1, 0, 1};
+
+int main() {
+    string commands;
+    cin >> commands;
+    int curDirection = 3;
+    int curX = 0;
+    int curY = 0;
+    for(char command : commands){
+        if(command == 'L'){
+            curDirection = (curDirection - 1 + 4) % 4;
+        }
+        if(command == 'R'){
+             curDirection = (curDirection  + 1) % 4;
+        }
+        if(command == 'F'){
+            curX += dx[curDirection];
+            curY += dy[curDirection];
+        }
+    }
+
+    cout << curX << ' ' << curY;
+    return 0;
+}
