00707c75c26eed307a0f9bed55bc8a011940097f
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-02-27 19:45:43 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240227/빙빙 돌며 사각형 채우기/snail-alphabet-square.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+char board[100][100];
+
+int n, m;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+bool isInRange(int x, int y){
+    return 0 <= x && x <= m-1
+    && 0 <= y && y <= n-1;
+}
+
+void drawAlphabet(){
+    int directionIndex = 0;
+    int loopCount = n * m;
+    char value = 'A';
+
+    int x = 0;
+    int y = 0;    
+    while(loopCount--){
+        board[y][x] = value;
+
+        int testX = x + dx[directionIndex];
+        int testY = y + dy[directionIndex];
+
+        if(!isInRange(testX, testY) || board[testY][testX]){
+            directionIndex = (directionIndex + 1) % 4;
+        }
+
+        x += dx[directionIndex];
+        y += dy[directionIndex];
+
+        if(value == 'Z'){
+            value = 'A';
+        }else{
+            value++;
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+
+    drawAlphabet();
+
+    for(int y = 0; y <= n-1; y++){
+        for(int x = 0; x <= m-1; x++){
+            cout << board[y][x] << ' ';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
