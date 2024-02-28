b5451f4c6fb9f465dab72a58886034a6c8af8b41
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-02-28 16:43:02 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240228/괄호 쌍 만들어주기 3/pair-parentheses-3.cpp
+++ 240228/괄호 쌍 만들어주기 3/pair-parentheses-3.cpp
@@ -8,9 +8,9 @@ int main() {
     cin >> str;
 
     int ans = 0;
-    int lastIndex = str.length();
-    for(int first = 0; first <= lastIndex-2; first++){
-        for(int second = first+1; second <= lastIndex-1; second++){
+    int lastIndex = str.length() - 1;
+    for(int first = 0; first <= lastIndex; first++){
+        for(int second = first+1; second <= lastIndex; second++){
             if(str[first] == '(' && str[second] == ')'){
                 ans++;    
             }
