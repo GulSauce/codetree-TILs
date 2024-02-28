7729b56f79b17c4a65b69747d91cd388bf638936
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-02-28 22:45:58 +0900

    Title: Carry 피하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240228/Carry 피하기 2/escaping-carry-2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int numbers[20];
+
+bool isMatchedCondition(int n1, int n2, int n3){
+    while(n1 && n2 && n3){
+        if(10 <= n1 % 10 + n2 % 10 + n3 % 10){
+            return false;
+        }
+        n1 /= 10;
+        n2 /= 10;
+        n3 /= 10;
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 0; i <= n; i++){
+        cin >> numbers[i];
+    }
+
+    int result = 0;
+    for(int i = 0; i <= n-1; i++){
+        for(int j = i+1; j <= n-1; j++){
+            for(int k = j+1; k <= n-1; k++){
+                if(isMatchedCondition(numbers[i], numbers[j], numbers[k])){
+                    result = max(result, numbers[i] + numbers[j] + numbers[k]);
+                }
+            }
+        }
+    }
+    cout << result;
+    return 0;
+}
