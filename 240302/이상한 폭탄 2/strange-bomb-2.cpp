585cf11e62e6045c97bd8a1b09ca606869fbe4de
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-03-02 19:58:00 +0900

    Title: 이상한 폭탄 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240302/이상한 폭탄 2/strange-bomb-2.cpp
+++ 240302/이상한 폭탄 2/strange-bomb-2.cpp
@@ -9,23 +9,17 @@ int main() {
     int N, K;
     cin >> N >> K;
 
-    memset(bombNumber, -1, sizeof bombNumber);
-
     for(int i = 0; i <= N-1; i++){
         cin >> bombNumber[i];
     }
 
     int maxBombNumber = -1;
-
     for(int i = 0; i <= N-K-1; i++){
         for(int j = i+1; j <= i+K; j++){
-            if(bombNumber[i] == -1){
-                continue;
-            }
             if(bombNumber[i] != bombNumber[j]){
                 continue;
             }
-            maxBombNumber = max(maxBombNumber, bombNumber[i]);
+            maxBombNumber = max(maxBombNumber, bombNumber[j]);
         }
     }
 
