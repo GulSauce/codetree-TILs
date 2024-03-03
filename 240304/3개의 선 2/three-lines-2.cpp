f4a4b1b01c6137e8a5161fda7546aafdecc01907
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-03-04 00:38:34 +0900

    Title: 3개의 선 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240304/3개의 선 2/three-lines-2.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+bool board[11][11];
+                
+bool checkIsThrowAll(int i, int j, int k){
+    int count = 0;
+    for(int p = 0; p <= 10; p++){
+        if(board[p][i] || board[p][j] || board[p][k]){
+            count++;
+        }
+    }
+    if(count == N){
+        return true;
+    }
+
+    count = 0;
+    for(int p = 0; p <= 10; p++){
+        if(board[p][i] || board[p][j] || board[k][p]){
+            count++;
+        }
+    }
+    if(count == N){
+        return true;
+    }
+    
+    count = 0;
+    for(int p = 0; p <= 10; p++){
+        if(board[p][i] || board[j][p] || board[k][p]){
+            count++;
+        }
+    }
+    if(count == N){
+        return true;
+    }
+
+    count = 0;
+    for(int p = 0; p <= 10; p++){
+        if(board[i][p] || board[j][p] || board[k][p]){
+            count++;
+        }
+    }
+    if(count == N){
+        return true;
+    }
+
+    return false;
+}
+
+int main() {
+    cin >> N;
+
+    for(int i = 0; i <= N-1; i++) {
+        int x, y;
+        cin >> x >> y;
+        board[y][x] = true;
+    }
+    
+    for(int i = 0; i <= 10; i++){
+        for(int j = 0; j <= 10; j++){
+            for(int k = 0; k <= 10; k++){
+                if(checkIsThrowAll(i, j, k)){
+                    cout << 1;
+                    return 0;
+                }
+            }
+        }
+    }
+
+    cout << 0;
+    return 0;
+}
