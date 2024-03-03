087084f8027dafdba37db4c434d5507c6dd5dda6
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-03-03 18:50:09 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240303/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.cpp
+++ 240303/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.cpp
@@ -7,14 +7,10 @@ int main() {
     cin >> A >> B >> C;
 
     int maxSum = 0;
-    for(int aCount = 0; aCount <= 1000; aCount++){
-        for(int bCount = 0; bCount <= 1000; bCount++){
-            int sum = aCount*A + bCount*B;
-            if(C + 1 <= sum){
-                continue;
-            }
-            maxSum = max(sum, maxSum);
-        }
+    for(int i = 1; i*A <= C; i++){
+        int aSum = i*A;
+        int bSum = (C-aSum) / B * B;
+        maxSum = max(aSum +  bSum, maxSum);
     }
 
     cout << maxSum;
