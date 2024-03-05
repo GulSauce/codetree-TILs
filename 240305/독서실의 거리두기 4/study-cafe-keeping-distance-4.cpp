d4f7dee3244915b1620af134d21ac51f2e4d7d1b
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-03-05 14:15:18 +0900

    Title: 독서실의 거리두기 4, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240305/독서실의 거리두기 4/study-cafe-keeping-distance-4.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+string libarySeat;
+
+int calcMinDistAfterSeat(int first, int second){
+    if(libarySeat[first] == '1' || libarySeat[second] == '1'){
+        return false;
+    }
+
+    libarySeat[first] = '1';
+    libarySeat[second] = '1';
+
+
+    int cnt = 0;
+    int seatPosition[100];
+    for(int i = 0; libarySeat[i] != '\0'; i++){
+        seatPosition[cnt++] = i;
+    }
+
+    int minDist = INT_MAX;
+    for(int i = 1; i <= cnt-1; i++){
+        int currentDist = seatPosition[i]-seatPosition[i-1];
+        minDist = min(minDist, currentDist);
+    }
+    
+    libarySeat[first] = '0';
+    libarySeat[second] = '0';
+
+    return minDist;
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    cin >> libarySeat;
+
+    int maxMinDist = 0;
+    for(int first = 0; first <= N-1; first++){
+        for(int second = first+1; second <= N-1; second++){
+            maxMinDist = max(maxMinDist, calcMinDistAfterSeat(first, second));
+        }
+    }
+
+    cout << maxMinDist;
+    return 0;
+}
