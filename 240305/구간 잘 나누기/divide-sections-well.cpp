bfd6798216abca884272e936d6b7b56546059d35
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-03-05 21:51:03 +0900

    Title: 구간 잘 나누기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240305/구간 잘 나누기/divide-sections-well.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+
+int numbers[100];
+
+bool checkIsValid(int maxSum){
+    int partitionCount = 0;
+    int currentSum = 0;
+    for(int i = 0; i <= n-1; i++){
+        int testSum = currentSum + numbers[i];
+        if(maxSum+1 <= testSum){
+            partitionCount++;
+            currentSum = numbers[i];
+            continue;
+        }
+        currentSum = testSum;
+    }
+    
+    if(m <= partitionCount){
+        return false;
+    }
+
+    return true;
+}
+
+int main() {
+    cin >> n >> m;
+
+    for(int i = 0; i <= n-1; i++){
+        cin >> numbers[i];
+    }
+
+    for(int i = 1; i <= 10000; i++){
+        if(checkIsValid(i)){
+            cout << i;
+            return 0;
+        }
+    }
+
+    return 0;
+}
