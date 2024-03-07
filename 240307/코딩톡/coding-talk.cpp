c828777a59a1624fc147b54ce6fc9576f30ca135
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-03-07 17:04:07 +0900

    Title: 코딩톡, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240307/코딩톡/coding-talk.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+class MessageInfo{
+    public:
+        char who;
+        int nonReadCount;
+
+        MessageInfo(char who, int nonReadCount){
+            this->who = who;
+            this->nonReadCount = nonReadCount;
+        }
+
+        MessageInfo(){}
+};
+
+MessageInfo messageInfoArray[101];
+
+int main() {
+    int n, m, p;
+    cin >> n >> m >> p;
+
+    bool whoRead[26] = {};
+
+    for(int i = 1; i <= m; i++){
+        char c;
+        int u;
+        cin >> c >> u;
+        messageInfoArray[i] = MessageInfo(c, u);
+    }
+
+    for(int i = p; i <= m; i++){
+        int alphabetIndex =  messageInfoArray[i].who - 'A';
+        whoRead[alphabetIndex] = true;
+    }
+
+    for(int i = 0; i <= n-1; i++){
+        if(whoRead[i]){
+            continue;
+        }
+        cout << (char)(i + 'A') << ' ';
+    }
+    return 0;
+}
