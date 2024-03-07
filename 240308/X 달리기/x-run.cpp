3d4f9ddcd59d9c9d93af0e88022d337a53b2448d
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-03-08 04:49:05 +0900

    Title: X 달리기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240308/X 달리기/x-run.cpp
+++ 240308/X 달리기/x-run.cpp
@@ -6,24 +6,26 @@ int main() {
     int X;
     cin >> X;
 
-    if(X <= 2){
+    if(X <= 3){
         cout << X;
         return 0;
     }
 
     X--;
+
     for(int t = 1; t <= X; t++){
-        int area = 1*t + t*t/4;
         if(t % 2 == 1){
             continue;
         }
+
+        int area = 1*t + t*t/4;
         if(area == X){
             cout << t + 1;
             break;
         }
 
-        int tPlus1 = t+2;
-        int testArea = tPlus1 + tPlus1*tPlus1/4;
+        int tPlus2 = t+2;
+        int testArea = tPlus2 + tPlus2*tPlus2/4;
         
         if(X + 1 <= testArea){
             cout << t + 1 + 1;
