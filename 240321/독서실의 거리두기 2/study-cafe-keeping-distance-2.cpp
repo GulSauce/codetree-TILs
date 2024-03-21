e53faa8a3fc2598cbb557c183318653f846079eb
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-03-21 21:41:23 +0900

    Title: 독서실의 거리두기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240321/독서실의 거리두기 2/study-cafe-keeping-distance-2.cpp
+++ 240321/독서실의 거리두기 2/study-cafe-keeping-distance-2.cpp
@@ -4,93 +4,90 @@
 using namespace std;
 
 int N;
-string str;
+string librarySeatInfo;
 
-int calcSitHere(){
-    int sitHere = -1;
-
+int calcWhereSit(){
+    int sitHere = 0;
     int maxDist = 0;
-    for(int i = 0; str[i] != '\0';){
-        if(str[i] == '0'){
+    for(int i = 0; librarySeatInfo[i] != '\0';){
+        if(librarySeatInfo[i] == '0'){
             i++;
             continue;
         }
-        int left = i;
-        i++;
-        while(str[i] == '0' && str[i] != '\0'){
-            i++;
+
+        int dist = 1;
+        while(librarySeatInfo[i+dist] == '0' && librarySeatInfo[i+dist] != '\0'){
+            dist++;
         }
-        if(str[i] == '\0'){
+
+        if(librarySeatInfo[i+dist] == '\0'){
             break;
         }
-        int right = i;
-
-        int currentDist = right-left;
-        if(maxDist + 1 <= currentDist){
-            maxDist = currentDist / 2;
-            sitHere = (right + left) / 2;
+        if(maxDist + 1 <= dist){
+            maxDist = dist;
+            sitHere = (i + i + dist)/2;
         }
+        i += dist;
     }
 
-    if(str[0] == '0'){
-        int right = 0;
-        while(str[right] == '0' && str[right] != '\0'){
-            right++;
+    if(librarySeatInfo[0] == '0'){
+        int dist = 1;
+        while(librarySeatInfo[dist] == '0' && librarySeatInfo[dist] != '\0'){
+            dist++;
         }
-        if(str[right] != '\0'){
-            int currentDist = right;
-            if(maxDist + 1 <= currentDist){
-                maxDist = currentDist;
-                sitHere = 0;
+        if(librarySeatInfo[dist] != '\0'){
+             if(maxDist + 1 <= dist){
+                maxDist = dist;
+                sitHere = dist/2;
             }
         }
     }
 
-    if(str[N-1] == '0'){
-        int left = N-1;
-        while(0 <= left && str[left] == '0'){
-            left--;
+    if(librarySeatInfo[N-1] == '0'){
+        int dist = 1;
+        while(0 <= N-1-dist && librarySeatInfo[N-1-dist] == '0'){
+            dist++;
         }
-        if(left != -1){
-            int currentDist = N-1 - left;
-            if(maxDist + 1 <= currentDist){
-                maxDist = currentDist;
-                sitHere = N-1;
+        if(0 <= N-1-dist){
+            if(maxDist + 1 <= dist){
+                maxDist = dist;
+                sitHere = (N-1 + N-1-dist)/2;
             }
         }
     }
-    
+
     return sitHere;
 }
 
 int main() {
     cin >> N;
-    cin >> str;
-    
-    int sitHere = calcSitHere();
 
-    str[sitHere] = '1';
+    cin >> librarySeatInfo;
 
-    int minMaxDist = INT_MAX;
-    for(int i = 0; str[i] != '\0';){
-        if(str[i] == '0'){
+    int sitHere = calcWhereSit();
+
+    librarySeatInfo[sitHere] = '1';
+
+    int minDist = INT_MAX;
+    for(int i = 0; librarySeatInfo[i] != '\0';){
+        if(librarySeatInfo[i] == '0'){
             i++;
             continue;
         }
-        int left = i;
-        i++;
-        while(str[i] == '0' && str[i] != '\0'){
-            i++;
+
+        int dist = 1;
+        while(librarySeatInfo[i+dist] == '0' && librarySeatInfo[i+dist] != '\0'){
+            dist++;
         }
-        if(str[i] == '\0'){
+        if(librarySeatInfo[i+dist] == '\0'){
             break;
         }
-        int right = i;
-
-        int currentDist = (right - left);
-        minMaxDist = min(minMaxDist, currentDist);
+        if(dist + 1 <= minDist){
+            minDist = dist;
+        }
+        i += dist;
     }
 
-    cout << minMaxDist;
+    cout << minDist;
     return 0;
 }
