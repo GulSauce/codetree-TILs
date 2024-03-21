53b9e120f2d65df02b856814ad7256898f1a4a10
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-03-21 17:42:53 +0900

    Title: 독서실의 거리두기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240321/독서실의 거리두기 2/study-cafe-keeping-distance-2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    string str;
+    cin >> str;
+    
+    int cnt = 0;
+    int personPosition[1000];
+
+    for(int i = 0; str[i] != '\0'; i++){
+        if(str[i] == '1'){
+            personPosition[cnt++] = i;
+        }
+    }
+
+    int maxDist = 0;
+    for(int i = 1; i <= cnt - 1; i++){
+        maxDist = max(personPosition[i] - personPosition[i-1], maxDist);
+    }
+
+    int result = maxDist/2;
+
+    if(personPosition[0] != 0){
+        result = max(result, personPosition[0] - 0);
+    }
+    if(personPosition[cnt-1] != str.length()-1){
+        result = max(result, (int)str.length()-1 - personPosition[cnt-1]);
+    }
+
+    cout << result;
+    return 0;
+}
