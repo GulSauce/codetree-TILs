43e7b035467dbeb1378564cc7cb9f2cab830da33
Young-je Oh <139232765+GulSauce@users.noreply.github.com>
2024-03-23 05:07:40 +0900

    Title: ABC 줄 세우기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240323/ABC 줄 세우기/abc-line-up.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+char alphabetSeq[26];
+
+bool isGoodPosition[26];
+
+int main() {
+    int n;
+    cin >> n;
+
+    memset(isGoodPosition, true, sizeof isGoodPosition);
+
+    for(int i = 0; i <= n-1; i++){
+        cin >> alphabetSeq[i];
+    }
+
+    for(int i = 0; i <= n-1; i++){
+        if(alphabetSeq[i] == i+'A'){
+            continue;
+        }
+
+        isGoodPosition[i] = false;
+    }
+
+    int result = 0;
+
+    int needComplexSwapCount = 0;
+
+    for(int i = 0; i <= n-1; i++){
+        if(isGoodPosition[i]){
+            continue;
+        }
+
+        int currentAlphabetToInt = alphabetSeq[i] - 'A';
+        if(alphabetSeq[currentAlphabetToInt] == i + 'A'){
+            isGoodPosition[i] = true;
+            isGoodPosition[currentAlphabetToInt] = true;
+            result++;
+            continue;
+        }
+        needComplexSwapCount++;
+    }
+
+    result += needComplexSwapCount - 1;
+
+    cout << result;
+    return 0;
+}
