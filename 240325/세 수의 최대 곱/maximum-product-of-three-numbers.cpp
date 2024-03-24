a61a3e987e1f068c22e2ab7dd359142e3e2d507e
Young-je Oh <139232765+GulSauce@users.noreply.github.com>
2024-03-25 05:04:11 +0900

    Title: 세 수의 최대 곱, Time: 39ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240325/세 수의 최대 곱/maximum-product-of-three-numbers.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int numbers[100000];
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 0; i <= n-1; i++){
+        cin >> numbers[i];
+    }
+
+    sort(numbers, numbers + n);
+
+    int maxNumber = -1 * INT_MAX;
+
+    maxNumber = max(maxNumber, numbers[n-1] * numbers[n-2] * numbers[n-3]);
+    maxNumber = max(maxNumber, numbers[n-1] * numbers[n-2] * numbers[0]);
+    maxNumber = max(maxNumber, numbers[n-1] * numbers[0] * numbers[1]);
+
+    cout << maxNumber;
+    return 0;
+}
