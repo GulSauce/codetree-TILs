b757155b5d7c25b4077a284306ae5856799378e2
Young-je Oh <139232765+GulSauce@users.noreply.github.com>
2024-03-24 21:39:08 +0900

    Title: L, R 그리고 B, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240324/L, R 그리고 B/l-r-and-b.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string board[10];
+
+    for(int y= 0; y <= 9; y++){
+        cin >> board[y];
+    }
+
+    pair<int, int> bPosition;
+    pair<int, int> lPosition;
+    pair<int, int> rPosition;
+
+    for(int y = 0; y <= 9; y++){
+        for(int x= 0; x <= 9; x++){
+            if(board[y][x]== 'B'){
+                bPosition = {x, y};
+            }
+            if(board[y][x]== 'L'){
+                lPosition = {x, y};
+            }
+            if(board[y][x]== 'R'){
+                rPosition = {x, y};
+            }
+        }
+    }
+
+    if(lPosition.first == bPosition.first){
+        int result = abs(lPosition.second-bPosition.second)-1;
+        if(rPosition.first == lPosition.first){
+            result += 3;
+        }
+        cout << result;
+        return 0;
+    }
+
+    if(lPosition.second == bPosition.second){
+        int result = abs(lPosition.first-bPosition.first)-1;
+        if(rPosition.second == lPosition.second){
+            result += 3;
+        }
+        cout << result;
+        return 0;
+    }
+
+    int result = abs(lPosition.first-bPosition.first)
+    + abs(lPosition.second-bPosition.second)
+    - 1;
+    
+    cout << result;
+    return 0;
+}
