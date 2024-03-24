b16489cac39a1685c3fc98647a7168ecbf91c393
Young-je Oh <139232765+GulSauce@users.noreply.github.com>
2024-03-24 21:48:55 +0900

    Title: L, R 그리고 B, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240324/L, R 그리고 B/l-r-and-b.cpp
+++ 240324/L, R 그리고 B/l-r-and-b.cpp
@@ -9,45 +9,50 @@ int main() {
         cin >> board[y];
     }
 
-    pair<int, int> bPosition;
-    pair<int, int> lPosition;
-    pair<int, int> rPosition;
+    int bX, bY;
+    int lX, lY;
+    int rX, rY;
 
     for(int y = 0; y <= 9; y++){
         for(int x= 0; x <= 9; x++){
             if(board[y][x]== 'B'){
-                bPosition = {x, y};
+                bX = x;
+                bY = y;
             }
             if(board[y][x]== 'L'){
-                lPosition = {x, y};
+                lX = x;
+                lY = y;
             }
             if(board[y][x]== 'R'){
-                rPosition = {x, y};
+                rX = x;
+                rY = y;
             }
         }
     }
 
-    if(lPosition.first == bPosition.first){
-        int result = abs(lPosition.second-bPosition.second)-1;
-        if(rPosition.first == lPosition.first){
+    if(lX == bX){
+        int result = abs(lY-bY)-1;
+        if(rX == lX
+        && min(lY, bY) <= rY
+        && rY <= max(lY, bY)){
             result += 2;
         }
         cout << result;
         return 0;
     }
 
-    if(lPosition.second == bPosition.second){
-        int result = abs(lPosition.first-bPosition.first)-1;
-        if(rPosition.second == lPosition.second){
+    if(lY == bY){
+        int result = abs(lX-bX)-1;
+        if(rY == lY
+        && min(lX, bX) <= rX
+        && rX <= max(lX, bX)){
             result += 2;
         }
         cout << result;
         return 0;
     }
 
-    int result = abs(lPosition.first-bPosition.first)
-    + abs(lPosition.second-bPosition.second)
-    - 1;
+    int result = abs(lX-bX) + abs(lY-bY) - 1;
     
     cout << result;
     return 0;
