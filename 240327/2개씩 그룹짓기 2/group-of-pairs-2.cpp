98c25fe2bf2618920f9d56604af55564d275ddeb
Young-je Oh <139232765+GulSauce@users.noreply.github.com>
2024-03-27 03:55:48 +0900

    Title: 2개씩 그룹짓기 2, Time: 78ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240327/2개씩 그룹짓기 2/group-of-pairs-2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int numbers[100000];
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 0; i <= 2*n-1; i++){
+        cin >> numbers[i];
+    }
+
+    sort(numbers, numbers+2*n);
+
+    int maxMinDiff = INT_MAX;
+
+    for(int i = 0; i <= n-1; i++){
+        maxMinDiff = min(numbers[i+n] - numbers[i], maxMinDiff);
+    }
+
+    cout << maxMinDiff;
+    return 0;
+}
