1890e31919a7c4086430f180c619e8bc1ac47649
Young-je Oh <139232765+GulSauce@users.noreply.github.com>
2024-03-27 19:40:56 +0900

    Title: 황금비율 토스트, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240327/황금비율 토스트/golden-toast.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main() {
+    list<char> toasts;
+
+    int n, m;
+    cin >> n >> m;
+
+    string toastString;
+    cin >> toastString;
+
+    for(int i = 0; toastString[i] != '\0'; i++){
+        toasts.push_back(toastString[i]);
+    }
+
+    list<char>::iterator it = toasts.end();
+
+    while(m--){
+        char command;
+        cin >> command;
+        if(command == 'L'){
+            if(it == toasts.begin()){
+                continue;
+            }
+            it--;
+        }
+        if(command == 'R'){
+            if(it == toasts.end()){
+                continue;
+            }
+            it++;
+        }
+        if(command == 'D'){
+             if(it == toasts.end()){
+                continue;
+            }
+            toasts.erase(it);
+        }
+        if(command == 'P'){
+            char toast;
+            cin >> toast;
+
+            toasts.insert(it, toast);
+        }
+    }
+
+    for(it = toasts.begin(); it != toasts.end(); it++){
+        cout << *it;
+    }
+    return 0;
+}
