0cb2f3f46f75dc8ca4347bfd0724953fb22a2205
Young-je Oh <139232765+GulSauce@users.noreply.github.com>
2024-04-04 08:01:19 +0900

    Title: 힙 정렬 구현, Time: 10ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240404/힙 정렬 구현/implement-heap-sort.cpp
+++ 240404/힙 정렬 구현/implement-heap-sort.cpp
@@ -6,27 +6,33 @@ using namespace std;
 int n;
 int numbers[100002];
 
-void heapify(int startIndex, int arrayLength){
-    int parentValue = numbers[startIndex];
-    if(arrayLength + 1 <= startIndex*2){
+void heapify(int parentIndex, int arrayLength){
+    int parentValue = numbers[parentIndex];
+    int child1Value = -1;
+    int child2Value = -1;
+    int largestIndex = parentIndex;
+
+    if(parentIndex * 2 <= arrayLength){
+        child1Value = numbers[parentIndex*2];
+    }
+    if(parentIndex * 2 + 1<= arrayLength){
+        child2Value = numbers[parentIndex*2+1];
+    }
+
+    if(parentValue + 1 <= child1Value
+    && child2Value + 1<= child1Value){
+        swap(numbers[parentIndex], numbers[parentIndex*2]);
+        largestIndex = parentIndex*2;
+    }
+    if(parentValue + 1 <= child2Value
+    && child1Value + 1<= child2Value){
+        swap(numbers[parentIndex], numbers[parentIndex*2+1]);
+        largestIndex = parentIndex*2 + 1;
+    }
+    if(parentIndex == largestIndex){
         return;
     }
-
-    int child1Value = numbers[startIndex*2];
-    if(parentValue + 1 <= child1Value){
-        swap(numbers[startIndex], numbers[startIndex*2]);
-        heapify(startIndex*2, arrayLength);
-    }
-
-    if(arrayLength + 1 <= startIndex*2 + 1){
-        return;
-    }
-
-    int child2Value = numbers[startIndex*2+1];
-    if(parentValue + 1 <= child2Value){
-        swap(numbers[startIndex], numbers[startIndex*2+1]);
-        heapify(startIndex*2+1, arrayLength);
-    }
+    heapify(largestIndex, arrayLength);
 }
 
 int main() {
@@ -40,7 +46,7 @@ int main() {
         heapify(i, n);
     }
 
-    for(int i = n; i >= 1; i--){
+    for(int i = n; i >= 2; i--){
         swap(numbers[1], numbers[i]);
         heapify(1, i-1);
     }
