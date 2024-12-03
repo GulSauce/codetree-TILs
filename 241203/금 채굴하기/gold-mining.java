60c9d53ca62dbf99af1384ce2d2cdaab93eddfa8
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2024-12-03 19:34:31 +0900

    Title: 금 채굴하기, Time: 137ms, Memory: 10MB, Status: Runtime Error - CodeTree

--- 241203/금 채굴하기/gold-mining.java
+++ 241203/금 채굴하기/gold-mining.java
@@ -2,26 +2,26 @@ import java.util.*;
 
 public class Main {
     private static class Solver {
-        int[][] matrix;
+        int[][] goldMap;
         final int GOLD_PRICE;
-        final int MAX_INDEX;
+        final int MAX_ARRAY_LENGTH;
         boolean[][] isVisited;
 
         public  Solver(
-                int[][] matrix,
+                int[][] goldMap,
                 int goldPrice
         ){
-            this.matrix = matrix;
+            this.goldMap = goldMap;
             this.GOLD_PRICE = goldPrice;
-            MAX_INDEX = matrix[0].length - 1;
-            isVisited = new boolean[MAX_INDEX+1][MAX_INDEX+1];
+            MAX_ARRAY_LENGTH = goldMap[0].length;
+            isVisited = new boolean[MAX_ARRAY_LENGTH][MAX_ARRAY_LENGTH];
         }
 
         public  void solve(){
             int result = 0;
-            for(int y = 0; y <= MAX_INDEX; y++){
-                for(int x= 0; x <= MAX_INDEX; x++){
-                    for(int k = 1; k <= MAX_INDEX; k++){
+            for(int y = 0; y < MAX_ARRAY_LENGTH; y++){
+                for(int x = 0; x < MAX_ARRAY_LENGTH; x++){
+                    for(int k = 0; k < MAX_ARRAY_LENGTH; k++){
                         int goldCount = getGoldCount(x, y, k);
                         int kPrice = getKPrice(k);
                         if(kPrice <= goldCount*GOLD_PRICE){
@@ -34,11 +34,8 @@ public class Main {
         }
 
         private int getGoldCount(int centerX, int centerY, int moveCount){
-            for(boolean[] array: isVisited){
-                Arrays.fill(array, false);
-            }
-
-            return  moveAndGetGoldCount(centerY, centerX, moveCount);
+            initIsVisited();
+            return moveAndGetGoldCount(centerY, centerX, moveCount);
         }
 
         int moveAndGetGoldCount(int y, int x, int moveCount){
@@ -47,15 +44,21 @@ public class Main {
             }
 
             isVisited[y][x] = true;
-            return matrix[y][x] +
+            return goldMap[y][x] +
                     moveAndGetGoldCount(y-1, x, moveCount-1) +
-                    moveAndGetGoldCount(y, x-1, moveCount-1) +
                     moveAndGetGoldCount(y+1, x, moveCount-1) +
+                    moveAndGetGoldCount(y, x-1, moveCount-1) +
                     moveAndGetGoldCount(y, x+1, moveCount-1);
         }
 
+        private void initIsVisited(){
+            for(boolean[] array: isVisited){
+                Arrays.fill(array, false);
+            }
+        }
+
         private boolean isOutOfIndex(int i){
-            return i < 0 || MAX_INDEX < i;
+            return i < 0 || MAX_ARRAY_LENGTH < i;
         }
 
         private int getKPrice(int k){
@@ -67,13 +70,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         final int n = sc.nextInt();
         final int m = sc.nextInt();
-        int[][] matrix = new int[n][n];
+        int[][] goldMap = new int[n][n];
         for(int y = 0 ; y < n; y++){
             for(int x = 0; x < n; x++){
-                matrix[y][x] = sc.nextInt();
+                goldMap[y][x] = sc.nextInt();
             }
         }
 
-        new Solver(matrix, m).solve();
+        new Solver(goldMap, m).solve();
     }
 }
