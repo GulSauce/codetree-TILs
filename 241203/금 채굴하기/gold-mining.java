e060d164e176e834364b136797936229dbeae1a5
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2024-12-03 22:04:55 +0900

    Title: 금 채굴하기, Time: 138ms, Memory: 11MB, Status: Wrong Answer - CodeTree

--- 241203/금 채굴하기/gold-mining.java
+++ 241203/금 채굴하기/gold-mining.java
@@ -5,15 +5,15 @@ public class Main {
         int[][] goldMap;
         boolean[][] isVisited;
 
-        final int GOLD_PRICE;
+        final int UNIT_GOLD_PRICE;
         final int MAX_ARRAY_LENGTH;
 
         public  Solver(
                 int[][] goldMap,
-                int goldPrice
+                int unitGoldPrice
         ){
             this.goldMap = goldMap;
-            this.GOLD_PRICE = goldPrice;
+            this.UNIT_GOLD_PRICE = unitGoldPrice;
             MAX_ARRAY_LENGTH = goldMap[0].length;
             isVisited = new boolean[MAX_ARRAY_LENGTH][MAX_ARRAY_LENGTH];
         }
@@ -23,16 +23,22 @@ public class Main {
             int result = 0;
             for(int y = 0; y < MAX_ARRAY_LENGTH; y++){
                 for(int x = 0; x < MAX_ARRAY_LENGTH; x++){
-                    for(int k = 0; k < MAX_ARRAY_LENGTH; k++){
-                        int goldCount = getGoldCount(x, y, k);
-                        int kPrice = getKPrice(k);
-                        if(kPrice <= goldCount * GOLD_PRICE){
-                            result = Math.max(result, goldCount);
-                        }
-                    }
+                    result = Math.max(result, getGoldPriceWithIncreasingMoving(x, y));
                 }
             }
             System.out.print(result);
+        }
+
+        private int getGoldPriceWithIncreasingMoving(int centerX, int centerY){
+            int result = 0;
+            for(int moveCount = 0; moveCount < MAX_ARRAY_LENGTH; moveCount++){
+                int goldCount = getGoldCount(centerX, centerY, moveCount);
+                int movingPrice = getMovingPrice(moveCount);
+                if(movingPrice <= goldCount * UNIT_GOLD_PRICE){
+                    result = Math.max(result, goldCount);
+                }
+            }
+            return result;
         }
 
         private int getGoldCount(int centerX, int centerY, int moveCount){
@@ -41,16 +47,48 @@ public class Main {
         }
 
         int moveAndGetGoldCount(int x, int y, int moveCount){
-            if(moveCount == -1 || isOutOfIndex(y) || isOutOfIndex(x) || isVisited[y][x]){
-                return 0;
+            Queue<Coordinate> q = new LinkedList<>();
+            isVisited[y][x] = true;
+            q.add(new Coordinate(x, y));
+
+            int goldCount = 0;
+
+            while (!q.isEmpty()){
+                Coordinate currentCoordinate = q.poll();
+                int currentX = currentCoordinate.x;
+                int currentY = currentCoordinate.y;
+                goldCount += goldMap[currentY][currentX];
+
+                if(moveCount <= getDistFromCenter(new Coordinate(x, y), new Coordinate(currentX, currentY))){
+                    continue;
+                }
+
+                if(!isOutOfIndex(currentX + 1) && !isVisited[currentY][currentX+1]){
+                    isVisited[currentY][currentX+1] = true;
+                    q.add(new Coordinate(currentX+1, currentY));
+                }
+
+                if(!isOutOfIndex(currentX -1) && !isVisited[currentY][currentX-1]){
+                    isVisited[currentY][currentX-1] = true;
+                    q.add(new Coordinate(currentX-1, currentY));
+                }
+
+                if(!isOutOfIndex(currentY + 1) && !isVisited[currentY+1][currentX]){
+                    isVisited[currentY+1][currentX] = true;
+                    q.add(new Coordinate(currentX, currentY+1));
+                }
+
+                if(!isOutOfIndex(currentY - 1) && !isVisited[currentY-1][currentX]){
+                    isVisited[currentY-1][currentX] = true;
+                    q.add(new Coordinate(currentX, currentY-1));
+                }
             }
 
-            isVisited[y][x] = true;
-            return goldMap[y][x] +
-                    moveAndGetGoldCount(x, y-1, moveCount-1) +
-                    moveAndGetGoldCount(x, y+1, moveCount-1) +
-                    moveAndGetGoldCount(x-1, y, moveCount-1) +
-                    moveAndGetGoldCount(x+1 , y, moveCount-1);
+            return goldCount;
+        }
+
+        int getDistFromCenter(Coordinate center, Coordinate current){
+            return Math.abs(center.x - current.x) + Math.abs(center.y - current.y);
         }
 
         private void initIsVisited(){
@@ -63,8 +101,21 @@ public class Main {
             return i < 0 || MAX_ARRAY_LENGTH <= i;
         }
 
-        private int getKPrice(int k){
+        private int getMovingPrice(int k){
             return k * k + (k+1) * (k+1);
+        }
+
+        private static class Coordinate{
+            int x;
+            int y;
+
+            public  Coordinate(
+                    int x,
+                    int y
+            ){
+                this.x = x;
+                this.y = y;
+            }
         }
     }
 
