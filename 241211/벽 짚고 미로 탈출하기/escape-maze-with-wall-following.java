6c28e19ba307950c5c49014f7c291098b00414be
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2024-12-11 00:24:11 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 160ms, Memory: 11MB, Status: Passed - CodeTree

--- 241211/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.java
+++ 241211/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.java
@@ -28,6 +28,7 @@ public class Main {
                     time = -1;
                     break;
                 }
+                isVisited[currentCoordinate.y][currentCoordinate.x][headPosition] = true;
                 int nextY = currentCoordinate.y + dy[headPosition];
                 int nextX = currentCoordinate.x + dx[headPosition];
                 if (isOutOfRange(nextY, nextX)) {
@@ -64,7 +65,6 @@ public class Main {
 
         private void goFrom(int y, int x){
             currentCoordinate = new Coordinate(currentCoordinate.y + dy[headPosition], currentCoordinate.x + dx[headPosition]);
-            isVisited[y][x][headPosition] = true;
             time++;
         }
 
