5db5a463d226f59254ee6dc1221902e4743d52fb
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2024-12-24 03:00:57 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 3091ms, Memory: 60MB, Status: Time Limit Exceed - CodeTree

---
+++ 241224/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Main {
+    public static class Solver{
+        int maxNumber;
+        int totalRepeat;
+
+        List<Integer> combinations = new ArrayList<>();
+
+        public Solver(
+                int K,
+                int N
+        ){
+            this.maxNumber = K;
+            this.totalRepeat = N;
+        }
+
+        public void solve(){
+            printCombinations(1);
+        }
+
+        public void printCombinations(int repeatCount){
+            if(totalRepeat < repeatCount){
+                printCombination();
+                return;
+            }
+            for(int number = 1; number <= maxNumber; number++){
+                combinations.add(number);
+                printCombinations(repeatCount+1);
+                combinations.remove(combinations.size()-1);
+            }
+        }
+
+        private void printCombination(){
+            for(int number: combinations){
+                System.out.printf("%d ", number);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int K = sc.nextInt();
+        int N = sc.nextInt();
+        new Solver(K, N).solve();
+    }
+}
