40a08b09fc2205ceaa8725327c54a4a459bbde2c
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2024-12-24 03:25:41 +0900

    Title: 아름다운 수, Time: 124ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 241224/아름다운 수/beautiful-number.java
@@ -0,0 +1,79 @@
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
+public class Main {
+    private static class Solver{
+        int digit;
+        int result = 0;
+        List<Integer> numbers = new ArrayList<>();
+
+        public Solver(
+                int n
+        ){
+            this.digit = n;
+        }
+
+        private void solve(){
+            countBeautifulNumber(1);
+            System.out.print(result);
+        }
+
+        private void countBeautifulNumber(int repeatCount){
+            if(digit <= repeatCount){
+                printNumbers();
+                if(isBeautifulNumber()){
+                    System.out.println("beautiful");
+                    result++;
+                }
+                return;
+            }
+            for(int number = 1; number <= 4; number++){
+                numbers.add(number);
+                countBeautifulNumber(repeatCount+1);
+                numbers.remove(numbers.size()-1);
+            }
+        }
+
+        private boolean isBeautifulNumber(){
+            int targetNumber = numbers.get(0);
+            int targetNumberCount = 1;
+
+            for(int i = 1; i < numbers.size(); i++){
+                if(targetNumber == targetNumberCount){
+                    targetNumber = numbers.get(i);
+                    targetNumberCount = 1;
+                    continue;
+                }
+
+                int currentNumber = numbers.get(i);
+                if(targetNumber == currentNumber){
+                    targetNumberCount++;
+                    continue;
+                }
+                if(targetNumberCount != currentNumber){
+                    return false;
+                }
+                targetNumber = currentNumber;
+                targetNumberCount = 1;
+            }
+            if(targetNumberCount != targetNumber){
+                return false;
+            }
+
+            return true;
+        }
+
+        private void printNumbers(){
+            for(int number: numbers){
+                System.out.print(number);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        new Solver(n).solve();
+    }
+}
