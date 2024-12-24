f41e713a92d2db306d03996c4fbb64fb4d5ce392
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2024-12-24 18:14:55 +0900

    Title: 겹치지 않게 선분 고르기, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 241224/겹치지 않게 선분 고르기/select-segments-without-overlap.java
+++ 241224/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -1,8 +1,14 @@
+import javax.sound.sampled.Line;
 import java.util.*;
 
 public class Main {
     private static class Solver{
         List<LineInfo> lineInfos;
+        List<Integer> combinations = new ArrayList<>();
+        int combinationSize;
+        int maxNotCollision = 0;
+
+        boolean isFindFailure = true;
 
         public Solver(
                 List<LineInfo> lineInfos
@@ -11,34 +17,60 @@ public class Main {
         }
 
         public void solve(){
-            Collections.sort(lineInfos);
-            int currentEnd = lineInfos.get(0).end;
-            int count = 1;
-            for(int i = 1; i < lineInfos.size(); i++){
-                LineInfo lineInfo =  lineInfos.get(i);
-                if(lineInfo.start <= currentEnd){
+            for(int combinationSize = 1; combinationSize <= lineInfos.size(); combinationSize++){
+                this.combinationSize = combinationSize;
+                this.isFindFailure = true;
+                findNotCollisionLines(-1, 0);
+                if(isFindFailure){
                     continue;
                 }
-                currentEnd = lineInfo.end;
-                count++;
+                maxNotCollision = combinationSize;
             }
-            System.out.print(count);
+            System.out.print(maxNotCollision);
+        }
+
+        private void findNotCollisionLines(int lastChoiceIndex, int repeat){
+            if(combinationSize <= repeat){
+                if(isCollideAboveLines()){
+                    return;
+                }
+                isFindFailure = false;
+            }
+            for(int index = lastChoiceIndex+1; index < lineInfos.size(); index++){
+                combinations.add(index);
+                findNotCollisionLines(index, repeat+1);
+                combinations.remove(combinations.size()-1);
+            }
+        }
+
+        private boolean isCollideAboveLines(){
+            int[] lineCount = new int[1001];
+            for(int index: combinations){
+                LineInfo lineInfo = lineInfos.get(index);
+                for(int i = lineInfo.start; i <= lineInfo.end; i++){
+                    if(lineCount[i] == 1){
+                        return true;
+                    }
+                    lineCount[i]++;
+                }
+            }
+            return false;
         }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
         List<LineInfo> lineInfos = new ArrayList<>();
-        for(int i = 0; i < n; i++){
-            lineInfos.add(new LineInfo(sc.nextInt(), sc.nextInt()));
+        for(int i = 0; i < n; i++) {
+            int x1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            lineInfos.add(new LineInfo(x1, x2));
         }
-
         new Solver(lineInfos).solve();
     }
 
-    private static class LineInfo implements Comparable<LineInfo>{
+    private static class LineInfo{
         int start;
         int end;
 
@@ -48,11 +80,6 @@ public class Main {
         ){
             this.start = x1;
             this.end = x2;
-        }
-
-        @Override
-        public int compareTo(LineInfo lineinfo){
-            return this.end - lineinfo.end;
         }
     }
 }
