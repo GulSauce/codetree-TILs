44f29591b52fb40fa60fc39e1f9d6a6a0c0892f4
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-01-03 19:57:28 +0900

    Title: 단순한 동전 챙기기, Time: 148ms, Memory: 11MB, Status: Passed - CodeTree

--- 250103/단순한 동전 챙기기/collect-coins-easy.java
+++ 250103/단순한 동전 챙기기/collect-coins-easy.java
@@ -1,40 +1,35 @@
+import javax.swing.text.Style;
 import java.util.*;
 
 public class Main {
     private static class Solver{
+        int answer = Integer.MAX_VALUE;
+        int maxRepeat;
+
         List<String> grid;
+        List<Coin> coins;
+        List<Coin> selectedCoins = new ArrayList<>();
 
         Coordinate startCoordinate;
         Coordinate endCoordinate;
-
-        int maxGridIndex;
-        int prevCoinCount = 0;
-        int prevMoveCount = 0;
-
-        int answer = Integer.MAX_VALUE;
-
-        int[] dRow = {0, -1 , 0, 1};
-        int[] dCol = {1, 0, -1, 0};
-
-        boolean[][][] isVisited;
 
         public Solver(
                 List<String> grid
         ){
             this.grid = grid;
-            this.maxGridIndex = grid.size()-1;
             this.startCoordinate = getStartCoordinate();
             this.endCoordinate = getEndCoordinate();
-            this.isVisited = new boolean[maxGridIndex+1][maxGridIndex+1][4];
+            this.coins = getCoins();
+            this.maxRepeat = coins.size();
         }
 
-        public void solve() {
-            initIsVisited();
-            getCombination(startCoordinate, 0, 0);
-            printResult();
+        public void solve(){
+            Collections.sort(coins);
+            getCombination(-1, 0);
+            printAnswer();
         }
 
-        private void  printResult(){
+        private void printAnswer(){
             if(answer == Integer.MAX_VALUE){
                 System.out.println(-1);
             }else{
@@ -42,100 +37,122 @@ public class Main {
             }
         }
 
-        private void initIsVisited(){
-            for(boolean[][] arrays: isVisited){
-                for(boolean[] array: arrays){
-                    Arrays.fill(array, false);
+        private void getCombination(int prevIndex, int repeatCount){
+            if(repeatCount == maxRepeat){
+                int moveCount = getMoveCount();
+                if(moveCount == -1){
+                    return;
                 }
-            }
-        }
-
-        private void getCombination(Coordinate prevCoordinate, int prevCoinValue, int prevCoinCount){
-            if(prevCoordinate.row == endCoordinate.row && prevCoordinate.col == endCoordinate.col){
-                if(3 <= prevCoinCount){
-                    answer = Math.min(answer, prevMoveCount);
-                }
+                answer = Math.min(answer, moveCount);
                 return;
             }
 
-            for(int i = 0; i < 4; i++){
-                int curRow = prevCoordinate.row + dRow[i];
-                int curCol = prevCoordinate.col + dCol[i];
-
-                Coordinate curCoordinate = new Coordinate(curRow, curCol);
-                if(isOutOfGrid(curCoordinate)){
-                    continue;
-                }
-                if(isVisited[curRow][curCol][i]){
-                    continue;
-                }
-
-                int curCoinValue = prevCoinValue;
-                int curCoinCount = prevCoinCount;
-                if(isCoinAt(curCoordinate)) {
-                    int coinValue = grid.get(curRow).charAt(curCol) - '0';
-                    if (coinValue <= prevCoinValue) {
-                        continue;
-                    }
-                    curCoinCount++;
-                    curCoinValue = coinValue;
-                }
-
-
-                isVisited[curRow][curCol][i] = true;
-                prevMoveCount++;
-
-                getCombination(curCoordinate, curCoinValue, curCoinCount);
-
-                isVisited[curRow][curCol][i] = false;
-                prevMoveCount--;
+            for(int i = prevIndex+1; i < coins.size(); i++){
+                selectedCoins.add(coins.get(i));
+                getCombination(i, repeatCount+1);
+                selectedCoins.remove(selectedCoins.size()-1);
+                getCombination(i, repeatCount+1);
             }
         }
 
-        private boolean isCoinAt(Coordinate coordinate){
-            return '0' <= grid.get(coordinate.row).charAt(coordinate.col) && grid.get(coordinate.row).charAt(coordinate.col) <= '9';
+        private int getMoveCount(){
+            if(selectedCoins.isEmpty()){
+                return -1;
+            }
+
+            int coinCount = 1;
+            Coordinate firstCoordinate = new Coordinate(selectedCoins.get(0).coordinate.row, selectedCoins.get(0).coordinate.col);
+            int moveCount = getDist(startCoordinate, firstCoordinate);
+            Coordinate prevCoordinate = firstCoordinate;
+
+            for(int i = 1; i < selectedCoins.size(); i++){
+                Coordinate curCoordinate = selectedCoins.get(i).coordinate;
+                if(grid.get(curCoordinate.row).charAt(curCoordinate.col) - '0' <= grid.get(prevCoordinate.row).charAt(prevCoordinate.col) - '0'){
+                    return -1;
+                }
+                coinCount++;
+                moveCount += getDist(prevCoordinate, curCoordinate);
+                prevCoordinate = curCoordinate;
+            }
+
+            moveCount += getDist(prevCoordinate, endCoordinate);
+            if(coinCount < 3){
+                return -1;
+            }
+            return moveCount;
         }
 
-        private boolean isOutOfGrid(Coordinate coordinate){
-            return coordinate.row < 0 || maxGridIndex < coordinate.row || coordinate.col < 0 || maxGridIndex < coordinate.col;
+        private int getDist(Coordinate prevCoordinate, Coordinate curCoordinate){
+            return Math.abs(curCoordinate.row-prevCoordinate.row) + Math.abs(curCoordinate.col-prevCoordinate.col);
         }
 
         private Coordinate getStartCoordinate(){
             for(int row = 0; row < grid.size(); row++){
                 String str = grid.get(row);
-                for(int col = 0; col < str.length(); col++){
+                for(int col = 0; col <str.length(); col++){
                     if(str.charAt(col) == 'S'){
                         return new Coordinate(row, col);
                     }
                 }
             }
+
             return new Coordinate(-1, -1);
         }
 
         private Coordinate getEndCoordinate(){
             for(int row = 0; row < grid.size(); row++){
                 String str = grid.get(row);
-                for(int col = 0; col < str.length(); col++){
+                for(int col = 0; col <str.length(); col++){
                     if(str.charAt(col) == 'E'){
                         return new Coordinate(row, col);
                     }
                 }
             }
+
             return new Coordinate(-1, -1);
+        }
+
+        private List<Coin> getCoins(){
+            List<Coin> coins = new ArrayList<>();
+            for(int row = 0; row < grid.size(); row++){
+                String str = grid.get(row);
+                for(int col = 0; col < str.length(); col++){
+                    char charValue = str.charAt(col);
+                    if('0' <= charValue && charValue <= '9'){
+                        coins.add(new Coin(charValue-'0', new Coordinate(row, col)));
+                    }
+                }
+            }
+            return coins;
         }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int N = sc.nextInt();
         List<String> grid = new ArrayList<>();
         for(int i = 0; i < N; i++){
-            String str = sc.next();
-            grid.add(str);
+            grid.add(sc.next());
+        }
+        new Solver(grid).solve();
+    }
+
+    private static class Coin implements Comparable<Coin>{
+        int value;
+        Coordinate coordinate;
+
+        public Coin(
+                int value,
+                Coordinate coordinate
+        ){
+            this.value = value;
+            this.coordinate = coordinate;
         }
 
-        new Solver(grid).solve();
+        @Override
+        public int compareTo(Coin coin){
+            return this.value - coin.value;
+        }
     }
 
     private static class Coordinate{
