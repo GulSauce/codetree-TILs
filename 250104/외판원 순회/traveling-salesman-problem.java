c3f016f23685e3766df9bc8848c099256c49f6fe
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-01-04 12:09:16 +0900

    Title: 외판원 순회, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250104/외판원 순회/traveling-salesman-problem.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class Main {
+    private static class Solver{
+        int answer = Integer.MAX_VALUE;
+        int graphLength;
+        int maxNodeNumber;
+
+        int[][] graph;
+
+        boolean[] isVisited;
+
+        public Solver(
+                int[][] graph
+        ){
+            this.graph = graph;
+            this.graphLength = graph.length;
+            this.maxNodeNumber = graph.length-1;
+            this.isVisited = new boolean[graphLength];
+        }
+
+        public void solve(){
+            initIsVisited();
+            getPermutation(0, 0,1);
+            System.out.println(answer);
+        }
+
+        private void getPermutation(int prevRepeat, int prevSum, int prevNodeNumber){
+            if(prevRepeat == graphLength-2){
+                int curSum = prevSum + graph[prevNodeNumber][1];
+                answer = Math.min(answer, curSum);
+            }
+
+            for(int curNodeNumber = 2; curNodeNumber <= maxNodeNumber; curNodeNumber++){
+                if(isVisited[curNodeNumber]){
+                    continue;
+                }
+                isVisited[curNodeNumber] = true;
+                getPermutation(prevRepeat+1, prevSum+graph[prevNodeNumber][curNodeNumber], curNodeNumber);
+                isVisited[curNodeNumber] = false;
+            }
+        }
+
+        private void initIsVisited(){
+            Arrays.fill(isVisited, false);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] graph = new int[n+1][n+1];
+        for(int source = 1; source <= n; source++){
+            for(int dest = 1; dest <= n; dest++){
+                graph[source][dest] = sc.nextInt();
+            }
+        }
+
+        new Solver(graph).solve();
+    }
+}
