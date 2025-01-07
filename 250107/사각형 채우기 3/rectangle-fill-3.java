7fd7b880f256be67290ae1eb84479822c0c4a406
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-01-07 22:10:11 +0900

    Title: 사각형 채우기 3, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250107/사각형 채우기 3/rectangle-fill-3.java
+++ 250107/사각형 채우기 3/rectangle-fill-3.java
@@ -26,13 +26,19 @@ public class Main {
 
         private void calcDP(){
             for(int i =3; i <= width; i++){
-                dp[i] = dp[i-1] * 2 % R + dp[i-2] * 3 % R + dp[i-3] * 2 % R;
-                dp[i] = dp[i] % R;
+                dp[i] = dp[i-1] * 2 % R;
+                dp[i] += dp[i-2] * 3 % R;
+                dp[i] %= R;
+                for(int j = 1; j <= i-3; j++){
+                    dp[i] += dp[j] * 2 % R;
+                    dp[i] %= R;
+                }
+                dp[i] += 2;
+                dp[i] %= R;
             }
         }
 
         private void initDP(){
-            dp[0] = 1;
             dp[1] = 2;
             dp[2] = 7;
         }
