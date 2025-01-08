f215cf1387c1c9fa21a898e85f30563291b60bc6
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-01-08 14:40:32 +0900

    Title: 정수 사각형 최대 합, Time: 289ms, Memory: 21MB, Status: Passed - CodeTree

---
+++ 250108/정수 사각형 최대 합/maximum-sum-path-in-square.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Main {
+    private static class Solver{
+        int gridIndex;
+
+        int[][] dp;
+        int[][] grid;
+
+        public Solver(
+                int N,
+                int[][] grid
+        ){
+            this.gridIndex = N;
+            this.grid = grid;
+            this.dp = new int[N+1][N+1];
+        }
+
+        public void solve(){
+            initDP();
+            calcDP();
+            printAnswer();
+        }
+
+        private void printAnswer(){
+            System.out.println(dp[gridIndex][gridIndex]);
+        }
+
+        private void calcDP(){
+            for(int row = 2; row <= gridIndex; row++){
+                dp[row][1] = dp[row-1][1] + grid[row][1];
+                for(int col = 2; col <= gridIndex; col++){
+                    dp[row][col] = Math.max(dp[row][col-1]+grid[row][col], dp[row-1][col]+grid[row][col]);
+                }
+            }
+        }
+
+        private void initDP(){
+            dp[1][1] = grid[1][1];
+            for(int col = 2; col<=gridIndex; col++){
+                dp[1][col] = grid[1][col] + dp[1][col-1];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[][] grid = new int[N+1][N+1];
+        for(int row = 1; row <= N; row++){
+            for(int col = 1; col <= N; col++){
+                grid[row][col] = sc.nextInt();
+            }
+        }
+
+        new Solver(N, grid).solve();
+    }
+}
