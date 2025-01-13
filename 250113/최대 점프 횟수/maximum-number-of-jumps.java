56b2b06cfff977d264ebeed8d6795049c88e9772
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-01-13 12:04:13 +0900

    Title: 최대 점프 횟수, Time: 135ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250113/최대 점프 횟수/maximum-number-of-jumps.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class Main {
+    private static class Solver{
+        int n;
+        int jumpDistancesIndex;
+
+        int[] dp;
+
+        List<Integer> jumpDistances;
+
+        public Solver(
+                int n,
+                List<Integer> jumpDistances
+        ){
+            this.n = n;
+            this.dp = new int[n];
+            this.jumpDistances = jumpDistances;
+            this.jumpDistancesIndex = jumpDistances.size()-1;
+        }
+
+        public void solve(){
+            initDP();
+            calcDP();
+            printAnswer();
+        }
+
+        private void printAnswer(){
+            int answer = 0;
+            System.out.println(answer);
+        }
+
+        private void calcDP(){
+            for(int cur = 1; cur <= jumpDistancesIndex; cur++){
+                for(int prev = 0; prev < cur; prev++){
+                    if(prev + jumpDistances.get(prev) < cur){
+                        continue;
+                    }
+                    dp[cur] = Math.max(dp[cur], dp[prev]+1);
+                }
+            }
+        }
+
+        private void initDP(){
+            Arrays.fill(dp, -1);
+            dp[0] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<Integer> jumpDistances = new ArrayList<>();
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            jumpDistances.add(sc.nextInt());
+        }
+        new Solver(n, jumpDistances).solve();
+    }
+}
