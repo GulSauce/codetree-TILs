7604299b84fd61fd6a11c9e467eab94e87515cea
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-01-14 11:30:01 +0900

    Title: 동전 거슬러주기, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250114/동전 거슬러주기/coin-change.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+public class Main {
+    private static class Solver{
+        int coinsIndex;
+        int targetValue;
+
+        int[] dp;
+        List<Integer> coins;
+
+        public Solver(
+                int N,
+                int M,
+                List<Integer> coins
+        ){
+            this.coinsIndex = N;
+            this.targetValue = M;
+            this.dp = new int[M+1];
+            this.coins = coins;
+        }
+
+        public void solve(){
+            initDP();
+            calcDP();
+            printAnswer();
+        }
+
+        private void printAnswer(){
+            System.out.println(dp[targetValue]);
+        }
+
+        private void calcDP(){
+            for(int curValue = 1; curValue <= targetValue; curValue++){
+                for(int coin: coins){
+                    int prevValue = curValue - coin;
+                    if(isOutOfTarget(prevValue)){
+                        continue;
+                    }
+                    dp[curValue] = Math.min(dp[curValue], dp[prevValue]+1);
+                }
+            }
+        }
+
+        private void initDP(){
+            Arrays.fill(dp, Integer.MAX_VALUE);
+            for(int coin: coins){
+                if(isOutOfTarget(coin)){
+                    continue;
+                }
+                dp[coin] = 1;
+            }
+        }
+
+        private boolean isOutOfTarget(int value){
+            return targetValue < value || value <= 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        List<Integer> coins = new ArrayList<>();
+        for(int i = 0; i < N; i++){
+            coins.add(sc.nextInt());
+        }
+        new Solver(N, M, coins).solve();
+    }
+}
