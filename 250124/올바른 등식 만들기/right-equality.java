954caf5993cbdb2779ac538166b6a8c30235fefc
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-01-24 13:26:44 +0900

    Title: 올바른 등식 만들기, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250124/올바른 등식 만들기/right-equality.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+public class Main {
+    private static class Solver{
+        int numbersIndex;
+        int targetNumber;
+        final int OFFSET = 20;
+        final int NOT_ALLOCATED = Integer.MIN_VALUE;
+
+        List<Integer> numbers;
+
+        int[][] dp;
+
+        public Solver(
+                int N,
+                int M,
+                List<Integer> numbers
+        ){
+            this.numbersIndex = N-1;
+            this.targetNumber = M;
+            this.numbers = numbers;
+            this.dp = new int[N][41];
+        }
+
+        public void solve(){
+            initDP();
+            calcDP();
+            printAnswer();
+        }
+
+        private void printAnswer(){
+            System.out.println(dp[numbersIndex][targetNumber+OFFSET]);
+        }
+
+        private void calcDP(){
+            for(int i = 1; i <= numbersIndex; i++){
+                int cur = numbers.get(i);
+                for(int j = -20; j <= 20; j++){
+                    if(-20 <= j-cur){
+                        dp[i][j-cur+OFFSET] += dp[i-1][j+OFFSET];
+                    }
+                    if(j+cur <= 20){
+                        dp[i][j+cur+OFFSET] += dp[i-1][j+OFFSET];
+                    }
+                }
+            }
+        }
+
+        private void initDP(){
+            for(int[] array: dp){
+                Arrays.fill(array, NOT_ALLOCATED);
+            }
+            dp[0][numbers.get(0)+OFFSET] = 1;
+            dp[0][-numbers.get(0)+OFFSET] = 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        List<Integer> numbers = new ArrayList<>();
+        for(int i = 0; i < N; i++){
+            numbers.add(sc.nextInt());
+        }
+
+        new Solver(N, M, numbers).solve();
+    }
+}
