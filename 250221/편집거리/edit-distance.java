0aa7533ec1ccc4cf1824773183d5622f72a9f0ff
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-02-21 18:08:04 +0900

    Title: 편집거리, Time: 190ms, Memory: 15MB, Status: Passed - CodeTree

--- 250221/편집거리/edit-distance.java
+++ 250221/편집거리/edit-distance.java
@@ -51,20 +51,35 @@ public class Main {
             for (int[] array : dp) {
                 Arrays.fill(array, NOT_ALLOCATED);
             }
+            if (A.charAt(0) == B.charAt(0)) {
+                dp[0][0] = 0;
+            } else {
+                dp[0][0] = 1;
+            }
 
-            for (int i = 0; i < A.length(); i++) {
+            for (int i = 1; i < A.length(); i++) {
                 if (A.charAt(i) == B.charAt(0)) {
                     dp[i][0] = i;
                 } else {
-                    dp[i][0] = i + 1;
+                    int insert = i + 1;
+                    int delete = dp[i - 1][0] + 1;
+                    int change = i + 1;
+                    dp[i][0] = Math.min(dp[i][0], insert);
+                    dp[i][0] = Math.min(dp[i][0], delete);
+                    dp[i][0] = Math.min(dp[i][0], change);
                 }
             }
 
-            for (int i = 0; i < B.length(); i++) {
+            for (int i = 1; i < B.length(); i++) {
                 if (B.charAt(i) == A.charAt(0)) {
                     dp[0][i] = i;
                 } else {
-                    dp[0][i] = i + 1;
+                    int insert = dp[0][i - 1] + 1;
+                    int delete = i + 1 + 1;
+                    int change = i + 1;
+                    dp[0][i] = Math.min(dp[0][i], insert);
+                    dp[0][i] = Math.min(dp[0][i], delete);
+                    dp[0][i] = Math.min(dp[0][i], change);
                 }
             }
         }
