517ff7b853eb8f7a466328f2e01fd3cec5ef19b8
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-02-26 15:15:17 +0900

    Title: 최장 공통 부분 수열 2, Time: 188ms, Memory: 14MB, Status: Passed - CodeTree

--- 250226/최장 공통 부분 수열 2/longest-common-sequence-2.java
+++ 250226/최장 공통 부분 수열 2/longest-common-sequence-2.java
@@ -35,7 +35,7 @@ public class Main {
 
         private void backTrack() {
             int i = A.length() - 1, j = B.length() - 1;
-            while (i > 0 || j > 0) {
+            while (i >= 0 && j >= 0) {
                 if (A.charAt(i) == B.charAt(j)) {
                     answers.add(A.charAt(i));
                     i--;
@@ -56,9 +56,7 @@ public class Main {
                 }
                 j--;
             }
-            if (A.charAt(0) == B.charAt(0)) {
-                answers.add(A.charAt(0));
-            }
+   
         }
 
         private void calcDP() {
