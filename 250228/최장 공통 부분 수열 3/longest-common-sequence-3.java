26dedf073901fd2e51e5afbff30a49a1e7fb59e6
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-02-28 12:33:52 +0900

    Title: 최장 공통 부분 수열 3, Time: 279ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250228/최장 공통 부분 수열 3/longest-common-sequence-3.java
+++ 250228/최장 공통 부분 수열 3/longest-common-sequence-3.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -5,19 +7,24 @@ public class Main {
 
     private static class Solver {
 
-        String A;
-        String B;
+        int AIndex;
+        int BIndex;
+        List<Integer> A;
+        List<Integer> B;
 
-        Stack<Character> answers = new Stack<>();
         int[][] dp;
 
         public Solver(
-            String A,
-            String B
+            int N,
+            int M,
+            List<Integer> A,
+            List<Integer> B
         ) {
+            this.AIndex = N - 1;
+            this.BIndex = M - 1;
             this.A = A;
             this.B = B;
-            this.dp = new int[A.length()][B.length()];
+            this.dp = new int[N][M];
         }
 
         public void solve() {
@@ -27,17 +34,12 @@ public class Main {
         }
 
         private void printAnswer() {
-            backTrack();
-            while (!answers.isEmpty()) {
-                System.out.print(answers.pop());
-            }
-        }
-
-        private void backTrack() {
-            int i = A.length() - 1, j = B.length() - 1;
+            int i = A.size() - 1;
+            int j = B.size() - 1;
+            Stack<Integer> numbers = new Stack<>();
             while (i >= 0 && j >= 0) {
-                if (A.charAt(i) == B.charAt(j)) {
-                    answers.add(A.charAt(i));
+                if (A.get(i) == B.get(j)) {
+                    numbers.add(A.get(i));
                     i--;
                     j--;
                     continue;
@@ -50,51 +52,67 @@ public class Main {
                     i--;
                     continue;
                 }
-                if (dp[i - 1][j] >= dp[i][j - 1]) {
+                if (dp[i - 1][j] > dp[i][j - 1]) {
                     i--;
                     continue;
                 }
                 j--;
             }
+            while (!numbers.isEmpty()) {
+                System.out.printf("%d ", numbers.pop());
+            }
         }
 
         private void calcDP() {
-            for (int i = 1; i < A.length(); i++) {
-                for (int j = 1; j < B.length(); j++) {
-                    if (A.charAt(i) == B.charAt(j)) {
+            for (int i = 1; i <= AIndex; i++) {
+                for (int j = 1; j <= BIndex; j++) {
+                    if (A.get(i) == B.get(j)) {
                         dp[i][j] = dp[i - 1][j - 1] + 1;
-                    } else {
-                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                        continue;
                     }
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
 
         private void initDP() {
-            if (A.charAt(0) == B.charAt(0)) {
+            if (A.get(0) == B.get(0)) {
                 dp[0][0] = 1;
             }
-            for (int i = 1; i < A.length(); i++) {
-                if (A.charAt(i) == B.charAt(0)) {
+
+            for (int i = 1; i <= AIndex; i++) {
+                if (A.get(i) == B.get(0)) {
                     dp[i][0] = 1;
-                } else {
-                    dp[i][0] = dp[i - 1][0];
+                    continue;
                 }
+                dp[i][0] = dp[i - 1][0];
             }
-            for (int i = 1; i < B.length(); i++) {
-                if (B.charAt(i) == A.charAt(0)) {
+
+            for (int i = 1; i <= BIndex; i++) {
+                if (A.get(0) == B.get(i)) {
                     dp[0][i] = 1;
-                } else {
-                    dp[0][i] = dp[0][i - 1];
+                    continue;
                 }
+                dp[0][i] = dp[0][i - 1];
             }
         }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String A = sc.next();
-        String B = sc.next();
-        new Main.Solver(A, B).solve();
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        List<Integer> A = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            A.add(sc.nextInt());
+        }
+
+        List<Integer> B = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            B.add(sc.nextInt());
+        }
+
+        new Main.Solver(N, M, A, B).solve();
     }
 }
