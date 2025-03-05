9a04c99d75681d359555b74b7ee6957c6a3adfc8
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-03-05 09:59:28 +0900

    Title: 코딩톡, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250305/코딩톡/coding-talk.java
@@ -0,0 +1,96 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    private static class Solver {
+
+        int memberIndex;
+        int targetChatIndex;
+
+        int chatsIndex;
+        List<Chat> chats;
+
+        List<String> readMembers = new ArrayList<>();
+        List<String> nonReadMembers = new ArrayList<>();
+
+        public Solver(
+            int N,
+            int M,
+            int p,
+            List<Chat> chats
+        ) {
+            this.memberIndex = N - 1;
+            this.chatsIndex = M - 1;
+            this.targetChatIndex = p - 1;
+            this.chats = chats;
+        }
+
+        public void solve() {
+            getReadMembers();
+            getNonReadMembers();
+            printAnswer();
+        }
+
+        private void printAnswer() {
+            for (String member : nonReadMembers) {
+                System.out.print(member + " ");
+            }
+        }
+
+        private void getNonReadMembers() {
+            boolean[] alphabetExist = new boolean[26];
+            for (String member : readMembers) {
+                alphabetExist[member.charAt(0) - 'A'] = true;
+            }
+            for (int i = 0; i <= memberIndex; i++) {
+                if (alphabetExist[i]) {
+                    continue;
+                }
+                nonReadMembers.add(String.valueOf((char) (i + 'A')));
+            }
+        }
+
+        private void getReadMembers() {
+            List<String> readMembers = new ArrayList<>();
+            Chat target = chats.get(targetChatIndex);
+            for (int i = 0; i < targetChatIndex; i++) {
+                if (chats.get(i).unread != target.unread) {
+                    continue;
+                }
+                readMembers.add(chats.get(i).sender);
+            }
+            for (int i = targetChatIndex; i <= chatsIndex; i++) {
+                readMembers.add(chats.get(i).sender);
+            }
+            this.readMembers = readMembers;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int p = sc.nextInt();
+        List<Chat> chats = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            chats.add(new Chat(sc.next(), sc.nextInt()));
+        }
+        new Solver(N, M, p, chats).solve();
+    }
+
+    private static class Chat {
+
+        String sender;
+        int unread;
+
+        public Chat(
+            String c,
+            int u
+        ) {
+            this.sender = c;
+            this.unread = u;
+        }
+    }
+}
