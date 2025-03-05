84226614b5e870e4686a756ea6e77615b56a490d
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-03-05 09:07:24 +0900

    Title: 최소 와이파이 수, Time: 3066ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

---
+++ 250305/최소 와이파이 수/minimum-number-of-wifi.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    private static class Solver {
+
+        int peopleIndex;
+        int wifiDist;
+        final int NOT_FOUND = -1;
+        final int NOT_EXIST = 0;
+        List<Integer> people;
+
+
+        public Solver(
+            int n,
+            int m,
+            List<Integer> people
+        ) {
+            this.peopleIndex = n - 1;
+            this.wifiDist = m;
+            this.people = people;
+        }
+
+        public void solve() {
+            int target = getTarget(0);
+            int wifiCount = 0;
+            while (true) {
+                target = getTarget(target + 2 * wifiDist);
+                if (target == NOT_FOUND) {
+                    break;
+                }
+                wifiCount++;
+            }
+            System.out.println(wifiCount);
+        }
+
+        private int getTarget(int start) {
+            for (int i = start; i <= peopleIndex; i++) {
+                if (people.get(i) == NOT_EXIST) {
+                    continue;
+                }
+                return i;
+            }
+            return NOT_FOUND;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        List<Integer> people = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            people.add(sc.nextInt());
+        }
+        new Solver(n, m, people).solve();
+    }
+}
