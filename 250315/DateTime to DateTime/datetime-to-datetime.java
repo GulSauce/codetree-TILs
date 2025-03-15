48474edea3b85a21af47c8507c5e63f97806e8f2
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-03-15 13:18:30 +0900

    Title: DateTime to DateTime, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 250315/DateTime to DateTime/datetime-to-datetime.java
+++ 250315/DateTime to DateTime/datetime-to-datetime.java
@@ -33,7 +33,7 @@ public class Main {
         }
 
         private void printAnswer(int totalMinuteDiff) {
-            if (totalMinuteDiff <= 0) {
+            if (totalMinuteDiff < 0) {
                 System.out.println(-1);
                 return;
             }
