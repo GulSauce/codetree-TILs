1bda8b8a93e7c01b68757b4cdf48220baf2a0151
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-03-20 00:03:05 +0900

    Title: 겹치지 않게 선분 고르기, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250320/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    private static class Solver {
+
+        int answer = 0;
+        List<LineInfo> lineInfos;
+        List<LineInfo> lineCombination = new ArrayList<>();
+
+        public Solver(
+            List<LineInfo> lineInfos
+        ) {
+            this.lineInfos = lineInfos;
+        }
+
+        public void solve() {
+            Collections.sort(lineInfos);
+            setLineCombinationCheckingNotCollide(-1);
+            System.out.println(answer);
+        }
+
+        private void setLineCombinationCheckingNotCollide(int prevIndex) {
+            if (lineCombination.size() == lineInfos.size()) {
+                return;
+            }
+            for (int curIndex = prevIndex + 1; curIndex < lineInfos.size(); curIndex++) {
+                lineCombination.add(lineInfos.get(curIndex));
+                if (isNotCollide()) {
+                    answer = Math.max(answer, lineCombination.size());
+                }
+                setLineCombinationCheckingNotCollide(curIndex);
+                lineCombination.remove(lineCombination.size() - 1);
+            }
+        }
+
+        private boolean isNotCollide() {
+            for (int i = 1; i < lineCombination.size(); i++) {
+                if (lineCombination.get(i).left <= lineCombination.get(i - 1).right) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<LineInfo> lineInfos = new ArrayList<>();
+        int N = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            lineInfos.add(new LineInfo(sc.nextInt(), sc.nextInt()));
+        }
+        new Main.Solver(lineInfos).solve();
+    }
+
+    private static class LineInfo implements Comparable<LineInfo> {
+
+        int left;
+        int right;
+
+        @Override
+        public int compareTo(LineInfo lineInfo) {
+            return this.left - lineInfo.left;
+        }
+
+        public LineInfo(
+            int l,
+            int r
+        ) {
+            this.left = l;
+            this.right = r;
+        }
+    }
+}
