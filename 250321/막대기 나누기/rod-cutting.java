4d5ce5aeb262a08ac36a61269ea7fb104673d005
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-03-21 01:00:07 +0900

    Title: 막대기 나누기, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250321/막대기 나누기/rod-cutting.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    private static class Solver {
+
+        int maxLength;
+        List<Integer> pricePerLengthInfos;
+
+        int[] dp;
+
+        public Solver(
+            int N,
+            List<Integer> pricePerLengthInfos
+        ) {
+            this.maxLength = N;
+            this.pricePerLengthInfos = pricePerLengthInfos;
+            this.dp = new int[N + 1];
+        }
+
+        public void solve() {
+            initDP();
+            calcDP();
+            printAnswer();
+        }
+
+        private void printAnswer() {
+            System.out.println(dp[maxLength]);
+        }
+
+        private void calcDP() {
+            for (int cur = 1; cur <= maxLength; cur++) {
+                for (int prev = 1; prev < cur; prev++) {
+                    dp[cur] = Math.max(dp[cur],
+                        dp[prev] + dp[cur - prev]);
+                }
+            }
+        }
+
+        private void initDP() {
+            for (int i = 1; i <= maxLength; i++) {
+                dp[i] = pricePerLengthInfos.get(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int N;
+        List<Integer> pricePerLengthInfos;
+
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        pricePerLengthInfos = new ArrayList<>();
+        pricePerLengthInfos.add(-1);
+        for (int i = 1; i <= N; i++) {
+            pricePerLengthInfos.add(sc.nextInt());
+        }
+        sc.close();
+
+        new Solver(N, pricePerLengthInfos).solve();
+    }
+}
