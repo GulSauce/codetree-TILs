3183fd729430e955a87cc83faa3ea395cd5664dd
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-03-21 19:43:39 +0900

    Title: 경험치를 빠르게 얻기, Time: 229ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 250321/경험치를 빠르게 얻기/gain-exp-quickly.java
@@ -0,0 +1,89 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    private static class Solver {
+
+        int questInfosIndex;
+        int targetExp;
+        final int NOT_ALLOCATED = Integer.MAX_VALUE;
+        List<QuestInfo> questInfos;
+
+        int[] dp;
+
+        public Solver(
+            int N,
+            int M,
+            List<QuestInfo> questInfos
+        ) {
+            this.questInfosIndex = N;
+            this.targetExp = M;
+            this.questInfos = questInfos;
+            this.dp = new int[M + 1];
+        }
+
+        public void solve() {
+            initDP();
+            calcDP();
+            printAnswer();
+        }
+
+        private void printAnswer() {
+            System.out.println(dp[targetExp]);
+        }
+
+        private void calcDP() {
+            for (QuestInfo questInfo : questInfos) {
+                for (int curExp = targetExp; curExp >= 1; curExp--) {
+                    if (curExp - questInfo.exp < 0) {
+                        dp[curExp] = Math.min(dp[curExp], questInfo.requiredTime);
+                        continue;
+                    }
+                    if (dp[curExp - questInfo.exp] == NOT_ALLOCATED) {
+                        continue;
+                    }
+                    dp[curExp] = Math.min(dp[curExp],
+                        dp[curExp - questInfo.exp] + questInfo.requiredTime);
+                }
+            }
+        }
+
+        private void initDP() {
+            Arrays.fill(dp, NOT_ALLOCATED);
+            dp[0] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int N;
+        int M;
+        List<QuestInfo> questInfos = new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            questInfos.add(new QuestInfo(sc.nextInt(), sc.nextInt()));
+        }
+        sc.close();
+
+        new Main.Solver(N, M, questInfos).solve();
+    }
+
+    private static class QuestInfo {
+
+        int exp;
+        int requiredTime;
+
+        public QuestInfo(
+            int e,
+            int t
+        ) {
+            this.exp = e;
+            this.requiredTime = t;
+        }
+    }
+}
