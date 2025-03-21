e5d4fd03665f3cc6ed8fcc6b8fc4c6b059618d55
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-03-21 18:17:07 +0900

    Title: 최대 합 분할, Time: 407ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

---
+++ 250321/최대 합 분할/maximum-sum-partition.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    private static class Solver {
+
+        int numbersIndex;
+        final int NOT_ALLOCATED = Integer.MIN_VALUE;
+        final int OFFSET = 1_000_000;
+        final int MAX_DIFF = 2_000_000;
+
+        List<Integer> numbers;
+
+        int[][] dp;
+
+        public Solver(
+            int N,
+            List<Integer> numbers
+        ) {
+            this.numbersIndex = N - 1;
+            this.numbers = numbers;
+            this.dp = new int[N][MAX_DIFF + 1];
+        }
+
+        public void solve() {
+            initDP();
+            calcDP();
+            printAnswer();
+        }
+
+        private void printAnswer() {
+            System.out.println(dp[numbersIndex][OFFSET]);
+        }
+
+        private void calcDP() {
+            for (int i = 1; i <= numbersIndex; i++) {
+                int currentValue = numbers.get(i);
+                for (int targetDiff = MAX_DIFF; targetDiff >= 0; targetDiff--) {
+                    if (targetDiff - currentValue < 0) {
+                        continue;
+                    }
+                    if (dp[i - 1][targetDiff - currentValue] == NOT_ALLOCATED) {
+                        continue;
+                    }
+                    dp[i][targetDiff] = Math.max(
+                        dp[i][targetDiff],
+                        dp[i - 1][targetDiff - currentValue] + currentValue);
+
+                    dp[i][targetDiff - 2 * currentValue] = Math.max(
+                        dp[i][targetDiff - 2 * currentValue],
+                        dp[i - 1][targetDiff - currentValue]);
+
+                    dp[i][targetDiff - currentValue] = Math.max(
+                        dp[i][targetDiff - currentValue],
+                        dp[i - 1][targetDiff - currentValue]);
+                }
+            }
+        }
+
+        private void initDP() {
+            for (int[] array : dp) {
+                Arrays.fill(array, NOT_ALLOCATED);
+            }
+            int firstValue = numbers.get(0);
+            dp[0][firstValue + OFFSET] = firstValue;
+            dp[0][-firstValue + OFFSET] = 0;
+            dp[0][0 + OFFSET] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int N;
+        List<Integer> numbers = new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            numbers.add(sc.nextInt());
+        }
+        sc.close();
+
+        new Solver(N, numbers).solve();
+    }
+}
