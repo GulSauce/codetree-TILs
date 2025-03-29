e38fd7c98157924e2acb51a9243a35b6530144b3
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-03-29 16:30:27 +0900

    Title: 서로 다른 BST 개수 세기, Time: 107ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250329/서로 다른 BST 개수 세기/number-of-unique-bst.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int N;
+
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        sc.close();
+
+        new Solver(N).solve();
+    }
+}
+
+
+class Solver {
+
+    int targetNodeCount;
+    int[] dp;
+
+    public Solver(
+        int N
+    ) {
+        this.targetNodeCount = N;
+        this.dp = new int[N + 1];
+    }
+
+    public void solve() {
+        initDP();
+        calcDP();
+        printAnswer();
+    }
+
+    private void printAnswer() {
+        System.out.println(dp[targetNodeCount]);
+    }
+
+    private void calcDP() {
+        for (int nodeCount = 2; nodeCount <= targetNodeCount; nodeCount++) {
+            int treeCount = 0;
+            int withoutHeadNodeCount = nodeCount - 1;
+            for (int leftNodeCount = 0; leftNodeCount <= withoutHeadNodeCount; leftNodeCount++) {
+                int rightNodeCount = withoutHeadNodeCount - leftNodeCount;
+                treeCount += dp[leftNodeCount] * dp[rightNodeCount];
+            }
+            dp[nodeCount] = treeCount;
+        }
+    }
+
+    private void initDP() {
+        dp[0] = 1;
+        dp[1] = 1;
+    }
+}
