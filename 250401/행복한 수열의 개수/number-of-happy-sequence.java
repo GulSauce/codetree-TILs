192e35a69e6b00a4df7a9ac0a12b0c8d9d22fb71
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-04-01 18:32:45 +0900

    Title: 행복한 수열의 개수, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250401/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int N, M;
+        int[][] grid;
+
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        grid = new int[N + 1][N + 1];
+        for (int y = 1; y <= N; y++) {
+            for (int x = 1; x <= N; x++) {
+                grid[y][x] = sc.nextInt();
+            }
+        }
+        sc.close();
+
+        new Solver(N, M, grid).solve();
+    }
+}
+
+class Solver {
+
+    int gridIndex;
+    int consecutiveCountToHappy;
+    int[][] grid;
+
+    public Solver(
+        int N,
+        int M,
+        int[][] grid
+    ) {
+        this.gridIndex = N;
+        this.consecutiveCountToHappy = M;
+        this.grid = grid;
+    }
+
+    public void solve() {
+        int answer = 0;
+        for (int col = 1; col <= gridIndex; col++) {
+            if (isHappyOnColumnAt(col)) {
+                answer++;
+            }
+        }
+        for (int row = 1; row <= gridIndex; row++) {
+            if (isHappyOnRowAt(row)) {
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+
+    private boolean isHappyOnColumnAt(int col) {
+        int maxConsecutiveCount = 0;
+        int consecutiveCount = 1;
+        for (int row = 2; row <= gridIndex; row++) {
+            if (grid[row - 1][col] == grid[row][col]) {
+                consecutiveCount++;
+            } else {
+                consecutiveCount = 1;
+            }
+            maxConsecutiveCount = Math.max(maxConsecutiveCount, consecutiveCount);
+        }
+        if (consecutiveCountToHappy <= maxConsecutiveCount) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isHappyOnRowAt(int row) {
+        int maxConsecutiveCount = 0;
+        int consecutiveCount = 1;
+        for (int col = 2; col <= gridIndex; col++) {
+            if (grid[row][col - 1] == grid[row][col]) {
+                consecutiveCount++;
+            } else {
+                consecutiveCount = 1;
+            }
+            maxConsecutiveCount = Math.max(maxConsecutiveCount, consecutiveCount);
+        }
+        if (consecutiveCountToHappy <= maxConsecutiveCount) {
+            return true;
+        }
+        return false;
+    }
+
+}
