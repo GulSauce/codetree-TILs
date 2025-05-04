8b6dea70483b0baf5579ee2fa4c4227b3da7437c
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-05-04 17:33:26 +0900

    Title: 겹치지 않는 선분 2, Time: 132ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250504/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int N;
+        List<Line> lines = new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            int x1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            lines.add(new Line(x1, x2));
+        }
+        sc.close();
+
+        new Solver(lines).solve();
+    }
+}
+
+class Solver {
+    List<Line> lines;
+
+    public Solver(
+            List<Line> lines
+    ) {
+        this.lines = lines;
+    }
+
+    public void solve() {
+        int answer = 0;
+        for (int i = 0; i < lines.size(); i++) {
+            Line line = lines.get(i);
+            for (int j = 0; j < lines.size(); j++) {
+                if (i == j) {
+                    continue;
+                }
+                Line other = lines.get(j);
+                if (line.isCollide(other)) {
+                    break;
+                }
+                if (j == lines.size() - 1) {
+                    answer++;
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
+
+class Line {
+    Coordinate start;
+    Coordinate end;
+
+    public Line(
+            int x1,
+            int x2
+    ) {
+        start = new Coordinate(x1, 0);
+        end = new Coordinate(x2, 1);
+    }
+
+    public boolean isCollide(Line other) {
+        if (start.x < other.start.x && end.x < other.end.x) {
+            return false;
+        }
+        if (other.start.x < start.x && other.end.x < end.x) {
+            return false;
+        }
+        return true;
+    }
+}
+
+class Coordinate {
+    int x;
+    int y;
+
+    public Coordinate(
+            int x,
+            int y
+    ) {
+        this.x = x;
+        this.y = y;
+    }
+}
