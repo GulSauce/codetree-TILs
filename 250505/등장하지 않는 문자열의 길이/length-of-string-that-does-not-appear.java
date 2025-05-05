82bd3aace86bdb0193a80c7e6c95df997b169619
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-05-05 18:04:27 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250505/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.java
+++ 250505/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.java
@@ -23,28 +23,13 @@ class Solver {
     }
 
     public void solve() {
+        int answer = string.length();
         for (int length = 1; length <= string.length() - 1; length++) {
             boolean hasSubString = false;
             for (int i = 0; i <= string.length() - length; i++) {
-                String subString = string.substring(i, i + length);
-                if (i == 0) {
-                    String right = string.substring(i + length);
-                    if (right.contains(subString)) {
-                        hasSubString = true;
-                        break;
-                    }
-                    continue;
-                }
-                if (i == string.length() - length) {
-                    String left = string.substring(0, i);
-                    if (left.contains(subString)) {
-                        hasSubString = true;
-                        break;
-                    }
-                    continue;
-                }
-                String right = string.substring(i + length);
                 String left = string.substring(0, i);
+                String subString = string.substring(i, i + length);
+                String right = string.substring(i + length);
                 if (right.contains(subString)) {
                     hasSubString = true;
                     break;
@@ -55,9 +40,10 @@ class Solver {
                 }
             }
             if (!hasSubString) {
-                System.out.println(length);
-                return;
+                answer = length;
+                break;
             }
         }
+        System.out.println(answer);
     }
 }
