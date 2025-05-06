2e843d3c2702502ebff20e7026a66ee8fa0c5a5c
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-05-06 19:06:27 +0900

    Title: 두 수의 합, Time: 123ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250506/두 수의 합/sum-of-two-num.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int N, K;
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            numbers.add(sc.nextInt());
+        }
+        sc.close();
+
+        new Solver(K, numbers).solve();
+    }
+}
+
+class Solver {
+    int targetSum;
+    ArrayList<Integer> numbers;
+
+    public Solver(
+            int K,
+            ArrayList<Integer> numbers
+    ) {
+        this.targetSum = K;
+        this.numbers = numbers;
+    }
+
+    public void solve() {
+        HashMap<Integer, Integer> numberCountMap = new HashMap<>();
+        ArrayList<Integer> queryNumbers = new ArrayList<>();
+        for (Integer number : numbers) {
+            if (numberCountMap.containsKey(number)) {
+                numberCountMap.put(number, numberCountMap.get(number) + 1);
+            } else {
+                queryNumbers.add(number);
+                numberCountMap.put(number, 1);
+            }
+        }
+        int answer = 0;
+        Collections.sort(numbers);
+        for (Integer queryNumber : numbers) {
+            int targetDiff = targetSum - queryNumber;
+            if (targetDiff < queryNumber) {
+                continue;
+            }
+            if (!numberCountMap.containsKey(targetDiff)) {
+                continue;
+            }
+            if (queryNumber == targetDiff) {
+                if (!numberCountMap.containsKey(targetDiff)) {
+                    continue;
+                }
+                int numberCount = numberCountMap.get(targetDiff);
+                if (numberCount == 1) {
+                    continue;
+                }
+                answer += numberCount * (numberCount - 1) / 2;
+                continue;
+            }
+            int targetDiffNumberCount = numberCountMap.get(targetDiff);
+            int queryNumberCount = numberCountMap.get(queryNumber);
+            answer += queryNumberCount * targetDiffNumberCount;
+        }
+        System.out.println(answer);
+    }
+}
