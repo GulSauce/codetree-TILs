44bb8cae2988f12cb26c15dc600197594fe63d8e
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-05-07 21:29:49 +0900

    Title: 데이터 비교, Time: 1958ms, Memory: 54MB, Status: Passed - Codetree

---
+++ 250507/데이터 비교/data-comparison.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int N;
+        ArrayList<Integer> A = new ArrayList<>();
+        int M;
+        ArrayList<Integer> B = new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            A.add(sc.nextInt());
+        }
+        M = sc.nextInt();
+        for (int i = 0; i < M; i++) {
+            B.add(sc.nextInt());
+        }
+
+        new Solver(A, B).solve();
+    }
+}
+
+class Solver {
+    ArrayList<Integer> A;
+    ArrayList<Integer> B;
+
+    public Solver(
+            ArrayList<Integer> A,
+            ArrayList<Integer> B
+    ) {
+        this.A = A;
+        this.B = B;
+    }
+
+    public void solve() {
+        HashSet<Integer> hashSet = new HashSet<>(A);
+        for (Integer queryNumber : B) {
+            if (hashSet.contains(queryNumber)) {
+                System.out.print(1);
+            } else {
+                System.out.print(0);
+            }
+            System.out.print(" ");
+        }
+    }
+}
