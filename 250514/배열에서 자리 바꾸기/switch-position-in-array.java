15d83ae0e97305282e327721b1a7c0339fcf1592
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-05-14 01:41:17 +0900

    Title: 배열에서 자리 바꾸기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250514/배열에서 자리 바꾸기/switch-position-in-array.java
@@ -0,0 +1,127 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int N;
+        int Q;
+        ArrayList<Command> commands = new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        Q = sc.nextInt();
+        for (int i = 0; i < Q; i++) {
+            commands.add(new Command(sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt()));
+        }
+        sc.close();
+
+        new Solver(N, commands).solve();
+    }
+}
+
+class Solver {
+    int maxNumber;
+    ArrayList<Command> commands;
+    Node[] nodes;
+
+    public Solver(
+            int maxNumber,
+            ArrayList<Command> commands
+    ) {
+        this.nodes = new Node[maxNumber + 2];
+        this.maxNumber = maxNumber;
+        this.commands = commands;
+    }
+
+    public void solve() {
+        Node cur = new Node(0);
+        nodes[0] = cur;
+        for (int i = 1; i <= maxNumber + 1; i++) {
+            cur.insertNext(new Node(i));
+            nodes[i] = cur.next;
+            cur = cur.next;
+        }
+
+        for (Command command : commands) {
+            Node secondTargetNode = nodes[command.aStart].prev;
+            insertNodeAfter(nodes[command.bEnd], nodes[command.aStart], nodes[command.aEnd]);
+            insertNodeAfter(secondTargetNode, nodes[command.bStart], nodes[command.bEnd]);
+        }
+
+        Node head = nodes[0];
+        printNode(head);
+    }
+
+    private void insertNodeAfter(Node target, Node start, Node end) {
+        Node prev = start.prev;
+        Node next = end.next;
+        prev.disconnectRight();
+        prev.connectRight(next);
+
+        Node targetNext = target.next;
+        target.connectRight(start);
+        end.connectRight(targetNext);
+    }
+
+    private void printNode(Node head) {
+        Node cur = head.next;
+        while (cur.next != null) {
+            System.out.print(cur.value + " ");
+            cur = cur.next;
+        }
+    }
+}
+
+class Node {
+    int value;
+    Node prev;
+    Node next;
+
+    public Node(
+            int value
+    ) {
+        this.value = value;
+    }
+
+    public void insertNext(Node target) {
+        target.prev = this;
+        target.next = this.next;
+
+        if (this.next != null) {
+            this.next.prev = target;
+        }
+        this.next = target;
+    }
+
+    public void connectRight(Node target) {
+        this.next = target;
+        target.prev = this;
+    }
+
+    public void disconnectRight() {
+        if (next != null) {
+            next.prev = null;
+        }
+        next = null;
+    }
+}
+
+class Command {
+    int aStart;
+    int aEnd;
+    int bStart;
+    int bEnd;
+
+    public Command(
+            int aStart,
+            int aEnd,
+            int bStart,
+            int bEnd
+    ) {
+        this.aStart = aStart;
+        this.aEnd = aEnd;
+        this.bStart = bStart;
+        this.bEnd = bEnd;
+    }
+
+}
