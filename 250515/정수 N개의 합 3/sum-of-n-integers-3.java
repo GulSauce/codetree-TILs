416fc4a92a0714a4df51312e1c630599a6c656b8
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-05-15 20:41:40 +0900

    Title: 정수 N개의 합 3, Time: 1081ms, Memory: 52MB, Status: Passed - Codetree

--- 250515/정수 N개의 합 3/sum-of-n-integers-3.java
+++ 250515/정수 N개의 합 3/sum-of-n-integers-3.java
@@ -2,69 +2,70 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
-    int N, K;
-    int[][] grid;
+    public static void main(String[] args) {
+        int N, K;
+        int[][] grid;
 
-    Scanner sc = new Scanner(System.in);
-    N = sc.nextInt();
-    K = sc.nextInt();
-    grid = new int[N + 1][N + 1];
-    for (int row = 1; row <= N; row++) {
-      for (int col = 1; col <= N; col++) {
-        grid[row][col] = sc.nextInt();
-      }
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+        grid = new int[N + 1][N + 1];
+        for (int row = 1; row <= N; row++) {
+            for (int col = 1; col <= N; col++) {
+                grid[row][col] = sc.nextInt();
+            }
+        }
+        sc.close();
+
+        new Solver(N, K, grid).solve();
     }
-    sc.close();
-
-    new Solver(N, K, grid).solve();
-  }
 }
 
 
 class Solver {
 
-  int gridLength;
-  int subGridSize;
+    int gridLength;
+    int subGridSize;
 
-  int[][] prefixSum;
-  int[][] grid;
+    int[][] prefixSum;
+    int[][] grid;
 
-  public Solver(
-      int gridLength,
-      int subGridSize,
-      int[][] grid
-  ) {
-    this.prefixSum = new int[gridLength + 1][gridLength + 1];
-    this.subGridSize = subGridSize;
-    this.gridLength = gridLength;
-    this.grid = grid;
-  }
-
-  public void solve() {
-    init();
-    int answer = 0;
-    for (int row = subGridSize; row < grid.length; row++) {
-      for (int col = subGridSize; col < grid.length; col++) {
-        answer = Math.max(answer, getSum(row, col));
-      }
+    public Solver(
+        int gridLength,
+        int subGridSize,
+        int[][] grid
+    ) {
+        this.prefixSum = new int[gridLength + 1][gridLength + 1];
+        this.subGridSize = subGridSize;
+        this.gridLength = gridLength;
+        this.grid = grid;
     }
-    System.out.println(answer);
-  }
 
-  private int getSum(int row, int col) {
-    return
-        prefixSum[row][col] - prefixSum[row - subGridSize][col] - prefixSum[row][col - subGridSize]
-            + prefixSum[row - subGridSize][col - subGridSize];
-  }
-
-  private void init() {
-    for (int row = 1; row < prefixSum.length; row++) {
-      for (int col = 1; col < prefixSum.length; col++) {
-        prefixSum[row][col] =
-            prefixSum[row - 1][col] + prefixSum[row][col - 1] - prefixSum[row - 1][col - 1]
-                + grid[row][col];
-      }
+    public void solve() {
+        init();
+        int answer = 0;
+        for (int row = subGridSize; row < grid.length; row++) {
+            for (int col = subGridSize; col < grid.length; col++) {
+                answer = Math.max(answer, getSum(row, col));
+            }
+        }
+        System.out.println(answer);
     }
-  }
+
+    private int getSum(int row, int col) {
+        return
+            prefixSum[row][col] - prefixSum[row - subGridSize][col] - prefixSum[row][col
+                - subGridSize]
+                + prefixSum[row - subGridSize][col - subGridSize];
+    }
+
+    private void init() {
+        for (int row = 1; row < prefixSum.length; row++) {
+            for (int col = 1; col < prefixSum.length; col++) {
+                prefixSum[row][col] =
+                    prefixSum[row - 1][col] + prefixSum[row][col - 1] - prefixSum[row - 1][col - 1]
+                        + grid[row][col];
+            }
+        }
+    }
 }
