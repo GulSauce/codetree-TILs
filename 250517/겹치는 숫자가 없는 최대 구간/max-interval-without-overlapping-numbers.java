bee48a3a851cf73f287bc95ddfd26be87662400a
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-05-17 01:24:15 +0900

    Title: 겹치는 숫자가 없는 최대 구간, Time: 338ms, Memory: 21MB, Status: Passed - Codetree

---
+++ 250517/겹치는 숫자가 없는 최대 구간/max-interval-without-overlapping-numbers.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        int N;
+        ArrayList<Integer> numbers = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers.add(Integer.parseInt(st.nextToken()));
+        }
+        br.close();
+
+        new Solver(numbers).solve();
+    }
+}
+
+class Solver {
+
+    int[] numberCount = new int[100_000 + 1];
+    ArrayList<Integer> numbers;
+
+    public Solver(
+        ArrayList<Integer> numbers
+    ) {
+        this.numbers = numbers;
+    }
+
+    public void solve() {
+        int prevJ = -1;
+        int answer = 0;
+        for (int i = 0; i < numbers.size(); i++) {
+            while (true) {
+                int curJ = prevJ + 1;
+                if (numbers.size() <= curJ) {
+                    break;
+                }
+                if (numberCount[numbers.get(curJ)] == 1) {
+                    break;
+                }
+                numberCount[numbers.get(curJ)]++;
+                prevJ = curJ;
+            }
+
+            answer = Math.max(answer, prevJ - i + 1);
+            numberCount[numbers.get(i)]--;
+        }
+        System.out.println(answer);
+    }
+}
