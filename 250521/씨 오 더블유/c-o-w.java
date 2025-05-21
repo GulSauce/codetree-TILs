7192414475dfcb2ca6f958375ed1c0a90f4a50c1
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-05-21 09:08:29 +0900

    Title: 씨 오 더블유, Time: 89ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250521/씨 오 더블유/c-o-w.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        int N;
+        String string;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        string = " " + st.nextToken();
+
+        new Solver(string).solve();
+    }
+}
+
+class Solver {
+
+    String string;
+
+    int[] leftCCount;
+    int[] rightWCount;
+
+    public Solver(
+        String string
+    ) {
+        this.string = string;
+        this.leftCCount = new int[string.length()];
+        this.rightWCount = new int[string.length() + 1];
+    }
+
+    public void solve() {
+        for (int i = 1; i < string.length(); i++) {
+            int value = string.charAt(i) == 'C' ? 1 : 0;
+            leftCCount[i] = leftCCount[i - 1] + value;
+        }
+        for (int i = string.length() - 1; i >= 1; i--) {
+            int value = string.charAt(i) == 'W' ? 1 : 0;
+            rightWCount[i] = rightWCount[i + 1] + value;
+        }
+
+        int answer = 0;
+        for (int i = 2; i < string.length() - 1; i++) {
+            if (string.charAt(i) != 'O') {
+                continue;
+            }
+            answer += leftCCount[i - 1] * rightWCount[i + 1];
+        }
+        System.out.println(answer);
+    }
+}
