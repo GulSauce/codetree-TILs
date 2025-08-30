0530ddcbea2c7c8ebec8f97149f8519ac1c4a3ef
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-08-30 12:30:26 +0900

    Title: 초기 수열 복원하기, Time: 122ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250830/초기 수열 복원하기/restore-initial-sequence.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int N;
+        List<Integer> numbers = new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        for (int i = 0; i < N - 1; i++) {
+            numbers.add(sc.nextInt());
+        }
+        sc.close();
+
+        new Solver(numbers).solve();
+    }
+}
+
+class Solver {
+    List<Integer> numbers;
+
+    public Solver(
+            List<Integer> numbers
+    ) {
+        this.numbers = numbers;
+    }
+
+    public void solve() {
+        List<Integer> answer = new ArrayList<>();
+        for (int start = 1; start <= 1000; start++) {
+            answer.clear();
+            boolean success = true;
+            boolean[] exitsOfNumber = new boolean[2001];
+            int cur = start;
+            for (Integer number : numbers) {
+                
+                if (exitsOfNumber[cur]) {
+                    success = false;
+                    break;
+                }
+                exitsOfNumber[cur] = true;
+                answer.add(cur);
+                int next = number - cur;
+                cur = next;
+            }
+            answer.add(cur);
+            if (success) {
+                break;
+            }
+        }
+        for (Integer number : answer) {
+            System.out.print(number + " ");
+        }
+    }
+}
