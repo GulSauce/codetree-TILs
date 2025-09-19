b205ce36b53662c9745b2762839c218344d1b750
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-09-19 12:17:38 +0900

    Title: 부분수열 여부 판단하기, Time: 255ms, Memory: 18MB, Status: Passed - Codetree

---
+++ 250919/부분수열 여부 판단하기/determine-subsequence.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        int N, M;
+        ArrayList<Integer> A = new ArrayList<>();
+        ArrayList<Integer> B = new ArrayList<>();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A.add(Integer.parseInt(st.nextToken()));
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            B.add(Integer.parseInt(st.nextToken()));
+        }
+        br.close();
+
+        new Solver(A, B).solve();
+    }
+}
+
+class Solver {
+
+    ArrayList<Integer> A;
+    ArrayList<Integer> B;
+
+    public Solver(
+        ArrayList<Integer> A,
+        ArrayList<Integer> B
+    ) {
+        this.A = A;
+        this.B = B;
+    }
+
+public void solve() {
+    int prevJ = -1;
+    boolean failed = false;
+    for (int i = 0; i < B.size(); i++) {
+        while (true) {
+            // 갱신 실패 조건: 이미 이전 인덱스에서 끝났음
+            if (A.size()-1 == prevJ) {
+                failed = true;
+                break;
+            }
+            // 갱신 실패 조건: 일치하는 경우 
+            if (A.get(prevJ+1).equals(B.get(i))) {
+                break;
+            }           
+	          int curJ = prevJ + 1;
+            prevJ = curJ;
+        }
+        // 매칭에 실패했는지 확인
+        if (failed) {
+            break;
+        }
+        // 매칭에 성공한 것이므로 prevJ 증가
+        prevJ++;
+    }
+    if (failed) {
+        System.out.println("No");
+    } else {
+        System.out.println("Yes");
+    }
+}
+}
