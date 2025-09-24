c9278af1147d8eafee71c184a3490f0538b6aada
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-09-25 08:39:09 +0900

    Title: 괄호 쌍 만들어주기, Time: 88ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250925/괄호 쌍 만들어주기/pair-parentheses.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String parentheses = st.nextToken();
+
+        new Solver(parentheses).solve();
+    }
+}
+
+class Solver {
+
+    String parentheses;
+
+    int MAX_LENGTH = 100_000;
+    int[] leftCount = new int[MAX_LENGTH];
+    int[] rightCount = new int[MAX_LENGTH];
+
+    public Solver(
+        String parentheses
+    ) {
+        this.parentheses = parentheses;
+    }
+
+    public void solve() {
+
+        for (int i = parentheses.length() - 2; i >= 0; i--) {
+            if (parentheses.substring(i, i + 2).equals("))")) {
+                rightCount[i] = rightCount[i + 1] + 1;
+            } else {
+                rightCount[i] = rightCount[i + 1];
+            }
+        }
+
+        long answer = 0;
+        for (int i = 2; i < parentheses.length() - 1; i++) {
+            if (!parentheses.substring(i - 2, i).equals("((")) {
+                continue;
+            }
+
+            answer += rightCount[i + 1];
+        }
+
+        System.out.println(answer);
+    }
+}
