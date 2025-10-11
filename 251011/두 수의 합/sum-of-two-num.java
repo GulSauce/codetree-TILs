1282852c4a8fa20b1a0786cb3ec78ed7855df56f
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-10-11 11:13:01 +0900

    Title: 두 수의 합, Time: 379ms, Memory: 22MB, Status: Passed - Codetree

---
+++ 251011/두 수의 합/sum-of-two-num.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        int N, K;
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers.add(Integer.parseInt(st.nextToken()));
+        }
+        br.close();
+
+        new Solver(K, numbers).solve();
+    }
+}
+
+class Solver {
+
+    int targetSum;
+    ArrayList<Integer> numbers;
+
+    public Solver(
+        int K,
+        ArrayList<Integer> numbers
+    ) {
+        this.targetSum = K;
+        this.numbers = numbers;
+    }
+
+    public void solve() {
+        int answer = 0;
+        HashMap<Integer, Integer> numberCountMap = new HashMap<>();
+        for (Integer number : numbers) {
+            int targetDiff = targetSum - number;
+            if (numberCountMap.containsKey(targetDiff)) {
+                answer += numberCountMap.get(targetDiff);
+            }
+            
+            numberCountMap.put(number, numberCountMap.getOrDefault(number, 0) + 1);
+        }
+
+        System.out.println(answer);
+    }
+}
