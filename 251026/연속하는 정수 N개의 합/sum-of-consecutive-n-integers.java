2b73adb4af9a121cde0084880f8391909fa1ea8c
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-10-26 01:39:12 +0900

    Title: 연속하는 정수 N개의 합, Time: 250ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251026/연속하는 정수 N개의 합/sum-of-consecutive-n-integers.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        long N, M;
+        List<Integer> numbers = new ArrayList<>();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers.add(Integer.parseInt(st.nextToken()));
+        }
+
+        new Solver(M, numbers).solve();
+
+    }
+
+}
+
+class Solver {
+
+    long targetSum;
+    List<Integer> numbers;
+
+    public Solver(
+        long M,
+        List<Integer> numbers
+    ) {
+        this.targetSum = M;
+        this.numbers = numbers;
+    }
+
+    public void solve() {
+        int i = 0;
+        int j = -1;
+        int answer = 0;
+        int curSum = 0;
+        while (true) {
+            while (true) {
+                if (targetSum <= curSum) {
+                    break;
+                }
+                j++;
+                if (numbers.size() <= j) {
+                    break;
+                }
+                curSum += numbers.get(j);
+                if (targetSum <= curSum) {
+                    break;
+                }
+            }
+            if (curSum == targetSum) {
+                answer++;
+            }
+            curSum -= numbers.get(i);
+            i++;
+            if (i == numbers.size()) {
+                break;
+            }
+        }
+        System.out.println(answer);
+    }
+
+}
