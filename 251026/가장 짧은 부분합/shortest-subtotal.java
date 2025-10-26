b46d20f93eae886fd6b09ab268447af2f8270c07
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-10-26 14:55:34 +0900

    Title: 가장 짧은 부분합, Time: 275ms, Memory: 21MB, Status: Wrong Answer - Codetree

---
+++ 251026/가장 짧은 부분합/shortest-subtotal.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int N, S;
+        List<Integer> numbers = new ArrayList<>();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers.add(Integer.parseInt(st.nextToken()));
+        }
+
+        new Solver(numbers, S).solve();
+    }
+}
+
+class Solver {
+
+    List<Integer> numbers;
+    int targetSum;
+
+    public Solver(
+        List<Integer> numbers,
+        int S
+    ) {
+        this.numbers = numbers;
+        this.targetSum = S;
+    }
+
+    public void solve() {
+        int i = 0;
+        int j = 0;
+        int answer = Integer.MAX_VALUE;
+        int sum = numbers.get(j);
+        for (i = 0; i < numbers.size(); i++) {
+            while (true) {
+                if (i <= j && targetSum <= sum) {
+                    answer = Math.min(answer, j - i + 1);
+                    break;
+                }
+                if (j == numbers.size() - 1) {
+                    break;
+                }
+                j++;
+                sum += numbers.get(j);
+            }
+            sum -= numbers.get(i);
+        }
+        System.out.println(answer);
+    }
+}
