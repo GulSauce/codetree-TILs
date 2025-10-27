69758fda0c47a4b74b6aa2654e5fa59ec35ea62b
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-10-28 02:16:17 +0900

    Title: 가장 짧은 부분합, Time: 90ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 251028/가장 짧은 부분합/shortest-subtotal.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int N, S;
+        List<Integer> numbers = new ArrayList<>();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers.add(Integer.parseInt(st.nextToken()));
+        }
+
+        new Solver(numbers, S).solve();
+    }
+}
+
+class Solver {
+
+    final int NOT_ALLOCATED = Integer.MAX_VALUE;
+    List<Integer> numbers;
+    int targetSum;
+
+    public Solver(
+        List<Integer> numbers,
+        int S
+    ) {
+        this.numbers = numbers;
+        this.targetSum = S;
+    }
+
+    public void solve() {
+        int i = 0;
+        int j = 1;
+        int answer = NOT_ALLOCATED;
+        int sum = numbers.get(0);
+        for (i = 0; i < numbers.size(); i++) {
+            while (true) {
+                if (j == numbers.size()) {
+                    break;
+                }
+                if (j <= i) {
+                    break;
+                }
+                sum += numbers.get(j);
+                if (targetSum <= sum) {
+                    answer = Math.min(answer, j - i + 1);
+                    break;
+                }
+                if (sum < targetSum) {
+                    j++;
+                }
+            }
+            sum -= numbers.get(i);
+        }
+        if (answer == NOT_ALLOCATED) {
+            System.out.println(-1);
+        } else {
+            System.out.println(answer);
+        }
+    }
+}
