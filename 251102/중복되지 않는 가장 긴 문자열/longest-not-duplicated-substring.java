60d9ee92f85d0a130e616d490008cc74d1b4d93a
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-11-02 18:09:38 +0900

    Title: 중복되지 않는 가장 긴 문자열, Time: 105ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251102/중복되지 않는 가장 긴 문자열/longest-not-duplicated-substring.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        String string;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        string = st.nextToken();
+
+        new Solver(string).solve();
+    }
+}
+
+class Solver {
+
+    String string;
+    HashMap<Character, Integer> charCountHashMap = new HashMap<>();
+
+    public Solver(String string) {
+        this.string = string;
+    }
+
+    public void solve() {
+        int i = 0;
+        int j = 0;
+        int answer = j - i + 1;
+        charCountHashMap.put(string.charAt(j), 1);
+        for (; i < string.length(); i++) {
+            while (true) {
+                if (j < i) {
+                    break;
+                }
+                if (2 <= charCountHashMap.get(string.charAt(j))) {
+                    break;
+                }
+                answer = Math.max(answer, j - i + 1);
+                if (j == string.length() - 1) {
+                    break;
+                }
+                j++;
+                Character curChar = string.charAt(j);
+                charCountHashMap.put(curChar, charCountHashMap.getOrDefault(curChar, 0) + 1);
+            }
+            Character curChar = string.charAt(i);
+            charCountHashMap.put(curChar, charCountHashMap.get(curChar) - 1);
+        }
+        System.out.println(answer);
+    }
+}
