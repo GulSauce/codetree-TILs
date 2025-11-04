01d7bb166243d8d0ca1864f585dc8b7822fac811
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-11-05 03:43:24 +0900

    Title: 수의 개수, Time: 761ms, Memory: 22MB, Status: Passed - Codetree

---
+++ 251105/수의 개수/number-of-integers.java
@@ -0,0 +1,88 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int N, M;
+        List<Integer> numbers = new ArrayList<>();
+        List<Integer> queries = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = toInt(st);
+        M = toInt(st);
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers.add(toInt(st));
+        }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            queries.add(toInt(st));
+        }
+
+        new Solver(numbers, queries).solve();
+    }
+
+    private static int toInt(StringTokenizer st) {
+        return Integer.parseInt(st.nextToken());
+    }
+}
+
+class Solver {
+
+    final int NOT_FOUND = -1;
+    List<Integer> numbers;
+    List<Integer> queries;
+
+    public Solver(List<Integer> numbers, List<Integer> queries) {
+        this.numbers = numbers;
+        this.queries = queries;
+    }
+
+    public void solve() {
+        for (Integer query : queries) {
+            int numberCount = upperBound(query) - lowerBound(query);
+            System.out.println(numberCount);
+        }
+    }
+
+    private int lowerBound(int query) {
+        int left = 0;
+        int right = numbers.size() - 1;
+        int answer = numbers.size();
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int found = numbers.get(mid);
+            if (query <= found) {
+                answer = Math.min(answer, mid);
+                right = mid - 1;
+            }
+            if (found < query) {
+                left = mid + 1;
+            }
+        }
+        return answer;
+    }
+
+    private int upperBound(int query) {
+        int left = 0;
+        int right = numbers.size() - 1;
+        int answer = numbers.size();
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int found = numbers.get(mid);
+            if (query < found) {
+                answer = Math.min(answer, mid);
+                right = mid - 1;
+            }
+            if (found <= query) {
+                left = mid + 1;
+            }
+        }
+        return answer;
+    }
+}
