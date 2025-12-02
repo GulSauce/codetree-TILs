6b5d4938de8b179fe63b0773d36b93ebd5014548
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-12-02 13:55:37 +0900

    Title: 공책과 포스트잇, Time: 93ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 251202/공책과 포스트잇/note-and-sticky-note.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int N, K, L;
+        List<Integer> writeCounts = new ArrayList<>();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = toInt(st);
+        K = toInt(st);
+        L = toInt(st);
+
+        st = new StringTokenizer(br.readLine());
+        writeCounts.add(-1);
+        for (int i = 0; i < N; i++) {
+            writeCounts.add(toInt(st));
+        }
+        br.close();
+
+        new Solver(K, L, writeCounts).solve();
+    }
+
+    private static int toInt(StringTokenizer st) {
+        return Integer.parseInt(st.nextToken());
+    }
+}
+
+class Solver {
+
+    int additionalPostItCount;
+    int writableCountPerPostIt;
+    List<Integer> writeCounts;
+
+    public Solver(int additionalPostItCount, int writableCountPerPostIt,
+        List<Integer> writeCounts) {
+        this.additionalPostItCount = additionalPostItCount;
+        this.writableCountPerPostIt = writableCountPerPostIt;
+        this.writeCounts = writeCounts;
+    }
+
+    public void solve() {
+        Collections.sort(writeCounts, Collections.reverseOrder());
+        int left = 0;
+        int right = 100_000;
+        int answer = left;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (isValid(mid)) {
+                left = mid + 1;
+                answer = Math.max(answer, mid);
+            } else {
+                right = mid - 1;
+            }
+        }
+        System.out.println(answer);
+    }
+
+    private boolean isValid(int hIndex) {
+        if (writeCounts.size() < hIndex) {
+            return false;
+        }
+        long lackCount = 0;
+        for (int i = 0; i < hIndex; i++) {
+            if (writeCounts.get(i) >= hIndex) {
+                continue;
+            }
+            lackCount += hIndex - writeCounts.get(i);
+        }
+        return lackCount <= (long) additionalPostItCount * writableCountPerPostIt;
+    }
+}
