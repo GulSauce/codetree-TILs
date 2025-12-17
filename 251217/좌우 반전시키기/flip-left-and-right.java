77194a4464ca9efbd55cd7c2d5ba6fa0384655b5
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-12-17 19:47:58 +0900

    Title: 좌우 반전시키기, Time: 90ms, Memory: 9MB, Status: Runtime Error - Codetree

---
+++ 251217/좌우 반전시키기/flip-left-and-right.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int N;
+        List<Integer> numbers = new ArrayList<>();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = toInt(st);
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers.add(toInt(st));
+        }
+
+        new Solver(numbers).solve();
+    }
+
+    private static int toInt(StringTokenizer st) {
+        return Integer.parseInt(st.nextToken());
+    }
+}
+
+class Solver {
+
+    final int FAILED = -1;
+    List<Integer> numbers;
+
+    public Solver(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public void solve() {
+        int pushCount = 0;
+        for (int i = 1; i < numbers.size() - 1; i++) {
+            if (numbers.get(i - 1) == 0) {
+                pushCount++;
+                numbers.set(i - 1, numbers.get(i - 1) ^ 1);
+                numbers.set(i, numbers.get(i) ^ 1);
+                numbers.set(i + 1, numbers.get(i + 1) ^ 1);
+            }
+        }
+        if (numbers.get(numbers.size() - 2) == 0) {
+            pushCount++;
+            numbers.set(numbers.size() - 2, numbers.get(numbers.size() - 2) ^ 1);
+            numbers.set(numbers.size() - 1, numbers.get(numbers.size() - 1) ^ 1);
+        }
+
+        System.out.println(numbers.get(numbers.size() - 1) == 0 ? FAILED : pushCount);
+    }
+}
