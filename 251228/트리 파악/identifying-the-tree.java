bbe243c844abc42419b2fd6166b11a8cf96b038a
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-12-28 00:53:08 +0900

    Title: 트리 파악, Time: 1654ms, Memory: 77MB, Status: Passed - Codetree

---
+++ 251228/트리 파악/identifying-the-tree.java
@@ -0,0 +1,93 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int N;
+        List<GraphMakeInfo> graphMakeInfos = new ArrayList<>();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = toInt(st);
+        for (int i = 0; i < N - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            graphMakeInfos.add(new GraphMakeInfo(toInt(st), toInt(st)));
+        }
+
+        new Solver(N, graphMakeInfos).solve();
+    }
+
+    private static int toInt(StringTokenizer st) {
+        return Integer.parseInt(st.nextToken());
+    }
+}
+
+class Solver {
+
+    int nodeCount;
+    HashSet<Integer> visitedHashSet = new HashSet<>();
+    HashSet<Integer> leafNodeHashSet = new HashSet<>();
+    HashMap<Integer, Integer> distHashMap = new HashMap<>();
+    HashMap<Integer, List<Integer>> graph = new HashMap<>();
+    List<GraphMakeInfo> graphMakeInfos;
+
+    public Solver(int nodeCount, List<GraphMakeInfo> graphMakeInfos) {
+        this.nodeCount = nodeCount;
+        this.graphMakeInfos = graphMakeInfos;
+    }
+
+    public void solve() {
+        for (GraphMakeInfo graphMakeInfo : graphMakeInfos) {
+            graph.computeIfAbsent(graphMakeInfo.start, (k) -> new ArrayList<>())
+                .add(graphMakeInfo.end);
+            graph.computeIfAbsent(graphMakeInfo.end, (k) -> new ArrayList<>())
+                .add(graphMakeInfo.start);
+        }
+
+        final int ROOT_NODE = 1;
+        distHashMap.put(ROOT_NODE, 0);
+        calcDistFindLeafDFS(ROOT_NODE);
+
+        long leafNodeDistSum = 0;
+        for (int leafNode : leafNodeHashSet) {
+            leafNodeDistSum += distHashMap.get(leafNode);
+        }
+        System.out.println(leafNodeDistSum % 2 == 0 ? 0 : 1);
+    }
+
+    private void calcDistFindLeafDFS(int cur) {
+        visitedHashSet.add(cur);
+        List<Integer> nextEdges = graph.get(cur);
+        boolean isLeaf = true;
+        for (int nextEdge : nextEdges) {
+            if (visitedHashSet.contains(nextEdge)) {
+                continue;
+            }
+            isLeaf = false;
+            distHashMap.put(nextEdge, distHashMap.get(cur) + 1);
+            calcDistFindLeafDFS(nextEdge);
+        }
+        if (isLeaf) {
+            leafNodeHashSet.add(cur);
+        }
+    }
+}
+
+class GraphMakeInfo {
+
+    int start;
+    int end;
+
+    public GraphMakeInfo(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+}
