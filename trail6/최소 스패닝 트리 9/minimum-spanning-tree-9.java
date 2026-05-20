88d9ee92ff4907ba7209698162226f3935a491dc
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2026-05-20 11:24:39 +0900

    [Passed] 최소 스패닝 트리 9 (1337ms, 52MB)

--- trail6/최소 스패닝 트리 9/minimum-spanning-tree-9.java
+++ trail6/최소 스패닝 트리 9/minimum-spanning-tree-9.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.PriorityQueue;
@@ -35,6 +36,8 @@ class Solver {
 
     int nodeCount;
     final int NOT_ALLOCATED = Integer.MAX_VALUE;
+
+    int[] dist;
     boolean[] intMST;
 
     List<Edge> edgeList;
@@ -44,6 +47,7 @@ class Solver {
     public Solver(int nodeCount, List<Edge> edgeList) {
         this.nodeCount = nodeCount;
         this.edgeList = edgeList;
+        this.dist = new int[nodeCount + 1];
         this.intMST = new boolean[nodeCount + 1];
     }
 
@@ -56,24 +60,30 @@ class Solver {
         PriorityQueue<Node> nodes = new PriorityQueue<>(
             (a, b) -> Integer.compare(a.weight, b.weight));
         nodes.add(new Node(1, 0));
+        dist[1] = 0;
+        Arrays.fill(dist, NOT_ALLOCATED);
         int answer = 0;
         while (!nodes.isEmpty()) {
             Node cur = nodes.poll();
             if (intMST[cur.number]) {
                 continue;
             }
+            dist[cur.number] = cur.weight;
             intMST[cur.number] = true;
-            answer += cur.weight;
             List<Node> near = graph.get(cur.number);
             for (Node node : near) {
                 if (intMST[node.number]) {
                     continue;
                 }
                 int weight = node.weight;
+                dist[node.number] = Math.min(dist[node.number], weight);
                 nodes.add(new Node(node.number, weight));
             }
         }
 
+        for (int v = 1; v <= nodeCount; v++) {
+            answer += dist[v];
+        }
         System.out.println(answer);
     }
 }
