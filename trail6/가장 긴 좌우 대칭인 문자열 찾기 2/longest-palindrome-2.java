ad5678830354d47362083f0e1bcd18dd448ed588
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2026-06-05 18:58:29 +0900

    [Passed] 가장 긴 좌우 대칭인 문자열 찾기 2 (224ms, 14MB)

---
+++ trail6/가장 긴 좌우 대칭인 문자열 찾기 2/longest-palindrome-2.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        String target;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        target = st.nextToken();
+
+        new Solver(target).solve();
+    }
+}
+
+class Solver {
+
+    String target;
+    int centerIndex = 0;
+    int rightIndex = 0;
+    int[] radiusOf;
+
+    public Solver(String original) {
+        this.target = original;
+        this.target = changeToOdd(original);
+        this.radiusOf = new int[target.length()];
+    }
+
+    public void solve() {
+        for (int i = 0; i < target.length(); i++) {
+            int determinedRadius;
+            if (i >= rightIndex) {
+                determinedRadius = 0;
+            } else {
+                int mirrorIndex = centerIndex - (i - centerIndex);
+                int mirrorPalRadius = radiusOf[mirrorIndex];
+                determinedRadius = Math.min(mirrorPalRadius, rightIndex - i);
+            }
+            int localLeft = i - determinedRadius;
+            int localRight = i + determinedRadius;
+            while (true) {
+                if (localLeft == 0) {
+                    break;
+                }
+                if (localRight >= target.length() - 1) {
+                    break;
+                }
+                if (target.charAt(localLeft) != target.charAt(localRight)) {
+                    localLeft++;
+                    localRight--;
+                    break;
+                }
+                localLeft--;
+                localRight++;
+            }
+            radiusOf[i] = localRight - i;
+            rightIndex = localRight;
+            centerIndex = i;
+        }
+
+        int answer = 0;
+        for (int i = 0; i < target.length(); i++) {
+            answer = Math.max(answer, 2 * radiusOf[i] + 1);
+        }
+        System.out.println(answer / 2);
+    }
+
+    private String changeToOdd(String target) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < target.length(); i++) {
+            sb.append('#');
+            sb.append(target.charAt(i));
+        }
+        sb.append('#');
+        return sb.toString();
+    }
+}
